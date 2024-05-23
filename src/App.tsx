518fcaa50e0285c1861862bf5513a95b7684236f
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-23 17:37:24 +0900

    Fixed: 절대경로 지정 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './pages/main';
-import Login from './pages/login';
+import Main from '@/pages/main';
+import Login from '@/pages/login';
 
 function App() {
   return (
