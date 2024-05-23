64fc675bcc33cfdf485e6fad5ad7d802a9d4b3a4
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-23 16:34:21 +0900

    Feat: React-Router-dom@6 추가 및 eslint 룰 추가 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+import Login from './pages/login';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">안녕</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Main />} />
+          <Route path="login">
+            <Route index element={<Login />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
