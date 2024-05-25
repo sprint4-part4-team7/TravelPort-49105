584f3b40a7057b89cdeccb4618d1ac2ed689d5ec
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-05-25 16:47:20 +0900

    Refactor: 모달 배경(임시) 및 모달 외부 클릭 시 모달창 닫힘 구현

--- src/components/EditInfo.tsx
+++ src/components/EditInfo.tsx
@@ -1,5 +1,5 @@
 const EditInfo = () => {
-  return <h1>사용자 정보 수정 모달</h1>;
+  return <h1 className="bg-white p-16">사용자 정보 수정 모달</h1>;
 };
 
 export default EditInfo;
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -4,15 +4,25 @@ import ModalPortal from '@/utils/ModalPortal';
 type ModalProps = {
   isOpen: boolean;
   children: ReactNode;
+  closeModal: () => void;
 };
 
-const Modal = ({ isOpen, children }: ModalProps) => {
+const Modal = ({ isOpen, children, closeModal }: ModalProps) => {
   if (!isOpen) {
     return null;
   }
+
+  const modalClass =
+    'flex flex-col fixed top-1/2 left-1/2  bg-white rounded-8 z-50 transform -translate-x-1/2 -translate-y-1/2 px-28 py-32';
+
   return (
     <ModalPortal>
-      <div className="my-16 border-1 p-16">{children}</div>
+      <button
+        type="button"
+        className="fixed left-0 top-0 w-full h-full bg-black"
+        onClick={closeModal}
+      />
+      <div className={`${modalClass} ${isOpen ? 'block' : ''}`}>{children}</div>
     </ModalPortal>
   );
 };
