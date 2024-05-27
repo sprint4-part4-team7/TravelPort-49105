3c3bc222a1e75d8518ff04bf0a432faa9247f2db
byunjiin <jibyun5808@naver.com>
2024-05-27 17:01:58 +0900

    Fix: 이슈가 생기기 전으로 다 바꾸기

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
-function Main() {
-  return <h1 className="text-3xl font-bold underline">안녕</h1>;
-}
+const Main = () => {
+  return <h1 className="text-3xl font-bold underline">여긴메인페이지</h1>;
+};
 
 export default Main;
