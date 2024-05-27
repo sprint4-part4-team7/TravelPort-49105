da3b2b2b0a03cc3e6d2221a3d38163a8afc60208
byunjiin <jibyun5808@naver.com>
2024-05-27 17:01:58 +0900

    Fix: 이슈가 생기기 전으로 다 바꾸기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Main from '@/pages/Main';
-import MyPage from './pages/MyPage';
+import MyPage from '@/pages/MyPage';
+import Location from '@/pages/productRegist/location/Location';
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
           <Route index element={<Main />} />
           <Route path="login">
             <Route index element={<Login />} />
+          </Route>
+          <Route path="location">
+            <Route index element={<Location />} />
           </Route>
           <Route path="mypage">
             <Route index element={<MyPage />} />
