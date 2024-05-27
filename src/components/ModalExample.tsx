950702964df54eb65053ebd635f0e898626513ed
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-05-27 10:31:40 +0900

    Refactor: 모달 열림 변수명 엄밀화

--- src/components/ModalExample.tsx
+++ src/components/ModalExample.tsx
@@ -4,7 +4,7 @@ import useModal from '@/hooks/useModal';
 
 const ModalExample = () => {
   //  useModal 훅을 사용하여 isOpen, openModal, closeModal을 가져온다.
-  const { isOpen, openModal, closeModal } = useModal();
+  const { isModalOpen, openModal, closeModal } = useModal();
   // 모달 컴포넌트를 Modal 컴포넌트로 감싸고, isOpen과 closeModal을 props로 전달한다.
   // 모달 열림 버튼에 openModal 함수를 연결한다.
   return (
@@ -12,7 +12,7 @@ const ModalExample = () => {
       <button onClick={openModal} type="button" className="p-16 border-1">
         Open Modal
       </button>
-      <Modal isOpen={isOpen} closeModal={closeModal}>
+      <Modal isOpen={isModalOpen} closeModal={closeModal}>
         <EditInfo />
       </Modal>
     </div>
