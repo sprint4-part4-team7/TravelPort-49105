61ed9fe9b02d457c0ef95406f25db0031451c79e
eugene9851 <eugenelee980713@gmail.com>
2024-05-27 16:54:07 +0900

    Feat: searchbar 컴포넌트화

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,28 +1,13 @@
-import { SetStateAction, useState } from 'react';
-
-const cardLists = [
-  {
-    title: '스쿠버 다이빙',
-    description: '신나는 바닷속 체험',
-  },
-  {
-    title: '스쿠버플라잉',
-    description: '신나는 하늘 체험',
-  },
-];
-
-function SearchBar() {
-  const [search, setSearch] = useState('');
-  const onChange = (e: { target: { value: SetStateAction<string> } }) =>
-    setSearch(e.target.value);
-
-  const filteredTitles = cardLists.filter((filteredTitle) => {
-    return filteredTitle.title
-      .replace(' ', '')
-      .toLowerCase()
-      .includes(search.trim().toLowerCase());
-  });
-
+/* eslint-disable no-undef */
+interface SearchBarProps {
+  titles: {
+    title: string;
+    description: string;
+  }[];
+  search: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+function SearchBar({ titles, search, onChange }: SearchBarProps) {
   return (
     <>
       <input
@@ -31,11 +16,13 @@ function SearchBar() {
         onChange={onChange}
         className="border-solid border-1 border-black p-10 w-3/4"
       />
-      <div>
-        {filteredTitles.map((filteredTitle) => (
-          <div>{filteredTitle.title}</div>
-        ))}
-      </div>
+      {!!search.length && (
+        <div>
+          {titles.map((filteredTitle) => (
+            <div>{filteredTitle.title}</div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,34 @@
-import SearchBar from '@/components/common/SearchBar';
+/* eslint-disable no-undef */
+import { useState } from 'react';
+import SearchBar from '../components/common/SearchBar';
+
+const cardLists = [
+  {
+    title: '스쿠버 다이빙',
+    description: '신나는 바닷속 체험',
+  },
+  {
+    title: '스쿠버플라잉',
+    description: '신나는 하늘 체험',
+  },
+];
 
 function Main() {
-  return <SearchBar />;
+  const [search, setSearch] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearch(e.target.value);
+
+  const filteredTitles = cardLists.filter((filteredTitle) => {
+    return filteredTitle.title
+      .replace(' ', '')
+      .toLowerCase()
+      .includes(search.trim().toLowerCase());
+  });
+
+  return (
+    <SearchBar titles={filteredTitles} search={search} onChange={onChange} />
+    // <Card />
+  );
 }
 
 export default Main;
