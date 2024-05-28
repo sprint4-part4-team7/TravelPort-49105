527c4be38139c3bed92b738ef2dbd8ca9ce9b6ed
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-28 15:52:41 +0900

    Feat: InputBox 초기 기능 및 디자인 구현

--- src/components/common/InputBox.tsx
+++ src/components/common/InputBox.tsx
@@ -1,5 +1,36 @@
-const InputBox = () => {
-  return <input />;
+import React from 'react';
+
+type InputBoxProps = {
+  label: string;
+  inputType?: 'text' | 'password' | 'number';
+  value?: string | number;
+  placeholder?: string;
+  width?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void | undefined;
+};
+
+const InputBox = ({
+  label,
+  inputType = 'text',
+  value,
+  placeholder = '입력',
+  width = '100%',
+  onChange = undefined,
+}: InputBoxProps) => {
+  return (
+    <div className="flex flex-col gap-8" style={{ width }}>
+      <label className="text-3xl" htmlFor={inputType}>
+        {label}
+      </label>
+      <input
+        className="px-16 py-10 text-2xl border-solid border-1 border-black"
+        type={inputType}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default InputBox;
