fac0e38e3e34379dc7b46a3e63e76bda0cf208ff
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-05-28 11:32:30 +0900

    Refactor: twMerge를 통한 모달의 동적 스타일 생성

--- src/components/ModalExample.tsx
+++ src/components/ModalExample.tsx
@@ -7,12 +7,13 @@ const ModalExample = () => {
   const { isModalOpen, openModal, closeModal } = useModal();
   // 모달 컴포넌트를 Modal 컴포넌트로 감싸고, isOpen과 closeModal을 props로 전달한다.
   // 모달 열림 버튼에 openModal 함수를 연결한다.
+  // 모달 컴포넌트에 className을 추가하여 모달의 추가적 클래스를 조절한다.
   return (
     <div>
       <button onClick={openModal} type="button" className="p-16 border-1">
         Open Modal
       </button>
-      <Modal isOpen={isModalOpen} closeModal={closeModal}>
+      <Modal isOpen={isModalOpen} closeModal={closeModal} modal="w-900">
         <EditInfo />
       </Modal>
     </div>
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,19 +1,24 @@
 import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 import ModalPortal from '@/utils/ModalPortal';
 
 type ModalProps = {
   isOpen: boolean;
   children: ReactNode;
   closeModal: () => void;
+  modal?: string;
 };
 
-const Modal = ({ isOpen, children, closeModal }: ModalProps) => {
+const Modal = ({ isOpen, children, closeModal, modal = '' }: ModalProps) => {
   if (!isOpen) {
     return null;
   }
 
-  const modalClass =
-    'flex flex-col fixed top-1/2 left-1/2  bg-white rounded-8 z-50 transform -translate-x-1/2 -translate-y-1/2 px-28 py-32';
+  const modalBasicClass = `flex flex-col fixed top-1/2 left-1/2 bg-white rounded-8 z-50 transform -translate-x-1/2 -translate-y-1/2 px-28 py-32 ${isOpen ? 'block' : ''}`;
+
+  const modalClass = twMerge(modalBasicClass, modal);
+
+  console.log(modalClass);
 
   return (
     <ModalPortal>
@@ -21,7 +26,7 @@ const Modal = ({ isOpen, children, closeModal }: ModalProps) => {
         className="fixed left-0 top-0 w-full h-full bg-black-modal"
         onClick={closeModal}
       />
-      <div className={`${modalClass} ${isOpen ? 'block' : ''}`}>{children}</div>
+      <div className={modalClass}>{children}</div>
     </ModalPortal>
   );
 };
