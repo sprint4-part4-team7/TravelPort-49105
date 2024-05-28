9f34360dfdd582a22667ffdd847e396e5b679c5a
Hooni07 <gkxogns21@gmail.com>
2024-05-29 08:44:43 +0900

    Feat: login 폴더 생성 및 Google, Kakao, Naver redirect 페이지 초기 설정

---
+++ src/pages/login/GoogleRedirect.tsx
@@ -0,0 +1,9 @@
+const GoogleRedirect = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Google Redirect Page</h1>
+    </div>
+  );
+};
+
+export default GoogleRedirect;
---
+++ src/pages/login/NaverRedirect.tsx
@@ -0,0 +1,9 @@
+const NaverRedirect = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Naver Redirect Page</h1>
+    </div>
+  );
+};
+
+export default NaverRedirect;
