f2ad9ca86ad86487ab724b7e1d7404c927d07373
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-28 11:30:32 +0900

    Feat: TextBox 최소 기능 및 초기 디자인 구현

--- src/components/common/TextBox.tsx
+++ src/components/common/TextBox.tsx
@@ -1,5 +1,32 @@
-const TextBox = () => {
-  return <input />;
+import React from 'react';
+
+type TextBoxProps = {
+  labelName: string;
+  textLimit: number;
+  placeholder: string;
+  value: string;
+  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+const TextBox = ({
+  labelName,
+  textLimit,
+  placeholder,
+  value,
+  onChange,
+}: TextBoxProps) => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={labelName}>{labelName}</label>
+      <textarea
+        className="border-black border-solid border-1 w-300"
+        placeholder={placeholder}
+        maxLength={textLimit}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default TextBox;
