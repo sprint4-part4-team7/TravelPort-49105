1bfa96778f317ddb87028e12856b9ea0dcac5bee
박지윤 <tkrhkfld123@naver.com>
2024-05-28 17:18:49 +0900

    chore: 메인 페이지에 Header 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,12 @@
+import HeaderBar from '@/components/common/headerBar/HeaderBar';
+
 const Main = () => {
-  return <h1 className="text-3xl font-bold underline">여긴메인페이지</h1>;
+  return (
+    <div>
+      <HeaderBar userType="user" />
+      <h1 className="text-3xl font-bold underline">여긴메인페이지</h1>
+    </div>
+  );
 };
 
 export default Main;
