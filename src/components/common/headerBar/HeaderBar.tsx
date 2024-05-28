b2ec3a37485202437fef3e6a82581b28c039fc96
박지윤 <tkrhkfld123@naver.com>
2024-05-28 17:18:13 +0900

    feat: header bar 구현

---
+++ src/components/common/headerBar/HeaderBar.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import LoginUserHeaderBar from './LoginUserHeaderBar';
+import UnLoginUserHeaderBar from './UnLoginUserHeaderBar';
+
+interface HeaderBarProps {
+  userType: 'user' | 'partner'; // 유저 타입: 'user' 혹은 'partner'
+}
+
+const HeaderBar: React.FC<HeaderBarProps> = ({ userType }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+  return (
+    <div className="py-[4.5rem] px-[3.6rem] text-2xl font-bold">
+      <div className="flex items-center">
+        <div className="flex-1 ">
+          <div className="bg-pink-200 w-170 py-[1.5rem] px-[3.2rem]">LOGO</div>
+        </div>
+        <div className="flex-1 bg-[#F5F5F5]">
+          <div className="py-[1.5rem] px-[3.2rem]">서치바</div>
+        </div>
+        <div className="flex-1" />
+        <div className="flex justify-end flex-1 space-x-2">
+          {isLoggedIn ? (
+            <LoginUserHeaderBar
+              setIsLoggedIn={setIsLoggedIn}
+              userType={userType}
+            />
+          ) : (
+            <UnLoginUserHeaderBar />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBar;
---
+++ src/components/common/headerBar/UnLoginUserHeaderBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const UnLoginUserHeaderBar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="space-x-2">
+      <button
+        type="button"
+        className="bg-slate-300 py-[1.5rem] px-[3.2rem] rounded-[1.2rem]"
+      >
+        파트너 페이지
+      </button>
+      <button
+        type="button"
+        className="bg-[#3F57D6] text-white py-[1.5rem] px-[3.2rem] rounded-[1.2rem]"
+        onClick={() => navigate('./login')}
+      >
+        로그인 및 회원가입
+      </button>
+    </div>
+  );
+};
+
+export default UnLoginUserHeaderBar;
