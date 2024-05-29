0f9e3510f0476147f1340b3703690cac4d3dd232
byunjiin <jibyun5808@naver.com>
2024-05-29 18:36:00 +0900

    parent f017e0e4691e27d0c587504eff8864bbf5e32b72 author byunjiin <jibyun5808@naver.com> 1716975360 +0900 committer byunjiin <jibyun5808@naver.com> 1716988651 +0900
    Feat: 팝업형식으로 변경 및 빌딩이름 조건추가 기능
    
    Feat: 팝업형식으로 변경 및 빌딩이름 조건추가 기능
    
    Feat: 팝업형식으로 변경 및 빌딩이름 조건추가 기능
    
    Feat: 팝업형식으로 변경 및 빌딩이름 조건추가 기능

--- src/components/common/map/PostCode.tsx
+++ src/components/common/map/PostCode.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React from 'react';
-import DaumPostcodeEmbed from 'react-daum-postcode';
+import React, { useState } from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
 
 type PostCodeProps = {
   setX: React.Dispatch<React.SetStateAction<number>>;
@@ -9,6 +9,11 @@ type PostCodeProps = {
 };
 
 const PostCode = ({ setX, setY, setName }: PostCodeProps) => {
+  const open = useDaumPostcodePopup();
+
+  const [address, setAddress] = useState({ x: 0, y: 0, addressName: '' }); // postcode에서 받아올 x,y,주소를 저장할 변수
+  const [sideAddress, setSideAddress] = useState(''); // 상세주소 input에 들어갈 값(초기엔 받아온 빌딩이름, 수정시 변경한 텍스트)
+
   const handleComplete = (data: any) => {
     const fullAddress = data.address;
     const config = {
@@ -22,19 +27,66 @@ const PostCode = ({ setX, setY, setName }: PostCodeProps) => {
     axios.get(url, config).then(function handleResult(result) {
       // API호출
       if (result.data !== undefined || result.data !== null) {
+        // x,y가 있다면
         if (result.data.documents[0].x && result.data.documents[0].y) {
           // Kakao Local API로 검색한 주소 정보 및 위도, 경도값 저장
           // kakaomap에서 제공되는 x,y와 daum에서 제공하는 x,y의 개념이 다름(daum에선 x가 경도, y가 위도)
-
-          setX(Number(result.data.documents[0].road_address.y));
-          setY(Number(result.data.documents[0].road_address.x));
-          setName(result.data.documents[0].road_address.building_name);
+          setAddress({
+            x: Number(result.data.documents[0].road_address.y),
+            y: Number(result.data.documents[0].road_address.x),
+            addressName: result.data.documents[0].address.address_name,
+          });
+          // 빌딩 이름이 있다면
+          if (result.data.documents[0].road_address.building_name) {
+            setSideAddress(result.data.documents[0].road_address.building_name);
+          }
         }
       }
     });
   };
 
-  return <DaumPostcodeEmbed onComplete={handleComplete} />;
+  const handleClick = () => {
+    // 클릭시 검색팝업열림
+    open({ onComplete: handleComplete });
+  };
+
+  const changeBuildingName = (event: any) => {
+    setSideAddress(event?.target.value);
+  };
+
+  const onSubmit = () => {
+    // 최종적으로 보낼 정보를 저장(추후 서버에 보낼때도 사용)
+    setX(address.x);
+    setY(address.y);
+    setName(sideAddress);
+  };
+  return (
+    <div>
+      <label htmlFor="address">
+        <p>기본 주소 검색</p>
+        <input
+          id="address"
+          value={address.addressName}
+          type="text"
+          onClick={handleClick}
+          readOnly // 검색으론 쓸수있지만 직접 텍스트입력은 못함
+        />
+      </label>
+      <label htmlFor="sideAddress">
+        <p>상세 주소 입력</p>
+        {/* input엔 빌딩이름이 기본적으로 존재하면 들어가고, 파트너가 바꾸고 싶다면 변경가능하게 구현 */}
+        <input
+          id="sideAddress"
+          value={sideAddress}
+          type="text"
+          onChange={changeBuildingName}
+        />
+      </label>
+      <button type="submit" onClick={onSubmit}>
+        주소 정보 저장
+      </button>
+    </div>
+  );
 };
 
 export default PostCode;
