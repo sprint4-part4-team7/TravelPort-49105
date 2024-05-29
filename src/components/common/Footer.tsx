7c21e76a9ff77ae415da4e7b00c0a8a2c051e217
박지윤 <tkrhkfld123@naver.com>
2024-05-30 00:58:11 +0900

    style: footer font 크기 조정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,7 +6,7 @@ import insta from '@/assets/icons/instagram.svg';
 const Footer = () => {
   return (
     <footer className="w-full bg-[#F2F2F3] h-[6rem]">
-      <div className="flex items-center justify-between max-w-screen-xl px-[23.8rem] mx-auto pt-[3.75rem] text-[1.6rem]">
+      <div className="flex items-center justify-between max-w-screen-xl px-[23.8rem] mx-auto pt-[3.75rem] text-[1.5rem]">
         <div className="font-medium text-[#7D7986]">TravelPort - 2024</div>
 
         <div className="flex gap-8 font-normal">
