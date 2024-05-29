7c3d152362ef50a8d84d0220f1d06308923b8650
eugene9851 <eugenelee980713@gmail.com>
2024-05-29 15:20:56 +0900

    Style: 연관검색창 디자인 & 연관검색어 없을 때 UI 구현

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import searchIcon from '@/assets/images/search.svg';
 import useSearchData from '@/hooks/useSearchData';
@@ -29,14 +30,20 @@ const SearchBar = ({ cardLists }: SearchBarProps) => {
         />
       </div>
       {!!search.length && (
-        <div>
-          {filteredTitles.length > 10
-            ? filteredTitles
-                .slice(0, 10)
-                .map((filteredTitle) => <div>{filteredTitle.title}</div>)
-            : filteredTitles.map((filteredTitle) => (
-                <div>{filteredTitle.title}</div>
-              ))}
+        <div className="border-solid border-1 border-[#F5F5F5] rounded-8 p-20 pl-40 bg-[#F5F5F5] text-13">
+          {filteredTitles.length > 10 ? (
+            filteredTitles
+              .slice(0, 10)
+              .map((filteredTitle) => (
+                <div className="leading-[3rem]">{filteredTitle.title}</div>
+              ))
+          ) : filteredTitles.length > 0 ? (
+            filteredTitles.map((filteredTitle) => (
+              <div className="leading-[3rem]">{filteredTitle.title}</div>
+            ))
+          ) : (
+            <div>연관 검색어가 없습니다.</div>
+          )}
         </div>
       )}
     </>
