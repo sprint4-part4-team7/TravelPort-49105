affaa53e82ae0f529c937c287507f7366d28c6e2
박지윤 <tkrhkfld123@naver.com>
2024-05-29 13:20:35 +0900

    style: 레이아웃 내부에 헤더 삽입

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,13 +1,18 @@
 import React, { ReactNode } from 'react';
+import HeaderBar from '../headerBar/HeaderBar';
 
 interface LayoutProps {
   children: ReactNode;
+  userType: 'user' | 'partner';
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, userType }) => {
   return (
-    <div className="px-[3.6rem] pb-[4.5rem] mt-[14.4rem] overflow-hidden break-words whitespace-normal">
-      {children}
+    <div>
+      <HeaderBar userType={userType} />
+      <div className="px-[3.6rem] pb-[4.5rem] mt-[14.4rem] overflow-hidden break-words whitespace-normal">
+        {children}
+      </div>
     </div>
   );
 };
