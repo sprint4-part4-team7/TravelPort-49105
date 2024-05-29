2780c65c993731fc88b7e7fd88b9cc23ebf520fd
eugene9851 <eugenelee980713@gmail.com>
2024-05-29 23:13:32 +0900

    Feat: 날짜 데이터 받았을 때 로직 구현

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -4,7 +4,9 @@ import CalendarCustom from '../components/common/CalendarCustom';
 const Reservation = () => {
   const { selectedDate, setSelectedDate } = useCalendar();
 
-  const startDate = new Date(2024, 4, 4);
+  const mockDate = '2024-05-27T03:05:19.935Z';
+  const dateArray = mockDate.split('T')[0].split('-').map(Number);
+  const startDate = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
   const endDate = new Date(2024, 7, 10);
   const holiday = [0];
 
