10f771f02b7ae9385db5b7a57462b37564314372
eugene9851 <eugenelee980713@gmail.com>
2024-05-29 23:05:06 +0900

    Feat: 시작일, 종료일 구체화

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,26 +1,23 @@
-import { useState } from 'react';
+import useCalendar from '@/hooks/useCalendar';
 import CalendarCustom from '../components/common/CalendarCustom';
 
-type DatePiece = Date | null;
-type SelectedDate = DatePiece | [DatePiece, DatePiece];
-
 const Reservation = () => {
-  const [selectedDate, setSelectedDate] = useState<SelectedDate>(new Date());
-  console.log(selectedDate, setSelectedDate);
+  const { selectedDate, setSelectedDate } = useCalendar();
 
   const startDate = new Date(2024, 4, 4);
-  const maxStartDate =
-    new Date().getTime() >= startDate.getTime() ? new Date() : startDate;
   const endDate = new Date(2024, 7, 10);
-  const minEndDate =
-    new Date().getTime() >= endDate.getTime() ? new Date() : endDate;
+  const holiday = [0];
+
   return (
-    <CalendarCustom
-      startDate={maxStartDate}
-      endDate={minEndDate}
-      selectedDate={selectedDate}
-      setSelectedDate={setSelectedDate}
-    />
+    <div className="w-[30rem]">
+      <CalendarCustom
+        startDate={startDate}
+        endDate={endDate}
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+        holiday={holiday}
+      />
+    </div>
   );
 };
 
