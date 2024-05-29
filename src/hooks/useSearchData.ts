eaa3439c413d92dc7d736273f55079818bc3c466
eugene9851 <eugenelee980713@gmail.com>
2024-05-29 15:06:06 +0900

    Feat: 검색어로 필터링된 상품 데이터 구하는 hook 생성

---
+++ src/hooks/useSearchData.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable no-undef */
+import { useState } from 'react';
+
+type cardListsType = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+const useSearchData = (cardLists: cardListsType[]) => {
+  const [search, setSearch] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearch(e.target.value);
+
+  const filteredTitles = cardLists.filter((filteredTitle) => {
+    return filteredTitle.title
+      .replace(' ', '')
+      .toLowerCase()
+      .includes(search.trim().toLowerCase());
+  });
+
+  return {
+    onChange,
+    search,
+    filteredTitles,
+  };
+};
+
+export default useSearchData;
