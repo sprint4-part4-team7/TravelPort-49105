ae9d34ed98fcef0712d067685f6e0cf698ee5afd
eugene9851 <eugenelee980713@gmail.com>
2024-05-29 15:07:01 +0900

    Feat: 연관검색어 10개 이하로 뜨도록 구현

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,26 +1,42 @@
-/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import searchIcon from '@/assets/images/search.svg';
+import useSearchData from '@/hooks/useSearchData';
+
 interface SearchBarProps {
-  titles: {
+  cardLists: {
+    id: number;
     title: string;
     description: string;
   }[];
-  search: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
-const SearchBar = ({ titles, search, onChange }: SearchBarProps) => {
+const SearchBar = ({ cardLists }: SearchBarProps) => {
+  const { onChange, search, filteredTitles } = useSearchData(cardLists);
   return (
     <>
-      <input
-        type="text"
-        value={search}
-        onChange={onChange}
-        className="border-solid border-1 border-black p-10 w-3/4"
-      />
+      <div className="relative">
+        <input
+          type="text"
+          value={search}
+          onChange={onChange}
+          className="border-solid border-1 border-[#F5F5F5] p-10 rounded-12 pl-40 w-full
+          text-18 font-semibold bg-[#F5F5F5] placeholder:text-[#CBC9CF] outline-[#AFAEAC]"
+          placeholder="어디로 떠나 볼까요~?"
+        />
+        <img
+          src={searchIcon}
+          alt="검색아이콘"
+          className="absolute top-12 left-12"
+        />
+      </div>
       {!!search.length && (
         <div>
-          {titles.map((filteredTitle) => (
-            <div>{filteredTitle.title}</div>
-          ))}
+          {filteredTitles.length > 10
+            ? filteredTitles
+                .slice(0, 10)
+                .map((filteredTitle) => <div>{filteredTitle.title}</div>)
+            : filteredTitles.map((filteredTitle) => (
+                <div>{filteredTitle.title}</div>
+              ))}
         </div>
       )}
     </>
