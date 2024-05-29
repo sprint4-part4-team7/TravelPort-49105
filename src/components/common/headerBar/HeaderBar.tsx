3bed56cbe16e0faaa6e4cb50c0a0f5fdd2e18d0b
박지윤 <tkrhkfld123@naver.com>
2024-05-29 12:44:36 +0900

    feat: search바 적용

--- src/components/common/headerBar/HeaderBar.tsx
+++ src/components/common/headerBar/HeaderBar.tsx
@@ -1,21 +1,49 @@
+/* eslint-disable no-undef */
 import React, { useState } from 'react';
 import LoginUserHeaderBar from './LoginUserHeaderBar';
 import UnLoginUserHeaderBar from './UnLoginUserHeaderBar';
+import SearchBar from '../SearchBar';
 
 interface HeaderBarProps {
   userType: 'user' | 'partner'; // 유저 타입: 'user' 혹은 'partner'
 }
 
+const cardLists = [
+  {
+    title: '스쿠버 다이빙',
+    description: '신나는 바닷속 체험',
+  },
+  {
+    title: '스쿠버플라잉',
+    description: '신나는 하늘 체험',
+  },
+];
+
 const HeaderBar: React.FC<HeaderBarProps> = ({ userType }) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+  const [search, setSearch] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearch(e.target.value);
+
+  const filteredTitles = cardLists.filter((filteredTitle) => {
+    return filteredTitle.title
+      .replace(' ', '')
+      .toLowerCase()
+      .includes(search.trim().toLowerCase());
+  });
   return (
-    <div className="pb-[4.5rem]  text-2xl font-bold ">
+    <div className="pb-[4.5rem] text-2xl font-bold">
       <div className="flex items-center">
         <div className="flex-1 ">
           <div className="bg-pink-200 w-170 py-[1.5rem] px-[3.2rem]">LOGO</div>
         </div>
         <div className="flex-1 bg-[#F5F5F5]">
-          <div className="py-[1.5rem] px-[3.2rem]">서치바</div>
+          {/* <div className="py-[1.5rem] px-[3.2rem]">서치바</div> */}
+          <SearchBar
+            titles={filteredTitles}
+            search={search}
+            onChange={onChange}
+          />
         </div>
         <div className="flex-1" />
         <div className="flex justify-end flex-1 space-x-2">
