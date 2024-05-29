cee28d5ea2e6b84647daa7bf12364032cdcf64fe
박지윤 <tkrhkfld123@naver.com>
2024-05-29 13:20:35 +0900

    style: 레이아웃 내부에 헤더 삽입

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,8 @@
-import HeaderBar from '@/components/common/headerBar/HeaderBar';
 import Layout from '@/components/common/layout/Layout';
 
 const Main = () => {
   return (
-    <Layout>
-      <HeaderBar userType="user" />
+    <Layout userType="user">
       <h1 className="text-3xl font-bold underline">여긴메인페이지</h1>
     </Layout>
   );
