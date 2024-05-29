d2deef155d0aedc8eca5e22753fcf2c15903c426
eugene9851 <eugenelee980713@gmail.com>
2024-05-29 15:06:32 +0900

    Feat: searchbar 추상화

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,33 +1,42 @@
 /* eslint-disable no-undef */
-import { useState } from 'react';
+// import useSearchData from '@/hooks/useSearchData';
 import SearchBar from '../components/common/SearchBar';
 
 const cardLists = [
   {
+    id: 1,
     title: '스쿠버 다이빙',
     description: '신나는 바닷속 체험',
   },
   {
+    id: 2,
     title: '스쿠버플라잉',
     description: '신나는 하늘 체험',
+  },
+  {
+    id: 3,
+    title: '나이트 사파리 체험',
+    description: '밤에 동물들을 보세요',
+  },
+  {
+    id: 4,
+    title: '스킨스쿠버',
+    description: '피부로 느끼는 물놀이',
   },
 ];
 
 const List = () => {
-  const [search, setSearch] = useState('');
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setSearch(e.target.value);
-
-  const filteredTitles = cardLists.filter((filteredTitle) => {
-    return filteredTitle.title
-      .replace(' ', '')
-      .toLowerCase()
-      .includes(search.trim().toLowerCase());
-  });
+  // const { filteredTitles } = useSearchData(cardLists);
 
   return (
-    <SearchBar titles={filteredTitles} search={search} onChange={onChange} />
-    // <Card />
+    <>
+      <div className="w-[40rem]">
+        <SearchBar cardLists={cardLists} />
+      </div>
+      {/* {filteredTitles.map((item) => (
+        <Card key={item.id} info={item} />
+      ))} */}
+    </>
   );
 };
 
