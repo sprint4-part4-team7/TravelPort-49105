4628a0a1b4b8143e907a758a86f5873bdd04f429
eugene9851 <eugenelee980713@gmail.com>
2024-05-30 12:29:50 +0900

    Fix: 요일 데이터 타입 변경

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -8,7 +8,7 @@ const Reservation = () => {
   const dateArray = mockDate.split('T')[0].split('-').map(Number);
   const startDate = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
   const endDate = new Date(2024, 7, 10);
-  const holiday = [0];
+  const holiday = ['0'].map(Number);
 
   return (
     <div className="w-[30rem]">
