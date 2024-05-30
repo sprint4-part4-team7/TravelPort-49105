483f959455ed10f725dabc0ac8a4966e0c2a3fd4
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-05-30 14:50:58 +0900

    Refactor: Button, InputBox 공용 컴포넌트로 인풋, 버튼 교체

--- src/components/EditInfo.tsx
+++ src/components/EditInfo.tsx
@@ -1,6 +1,8 @@
-import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
 import { useUserStore } from '@/utils/zustand';
 import { initUserInfo } from '@/mocks/InfoMock';
+import Button from '@/components/common/Button';
+import InputBox from './common/InputBox';
 
 interface UserInfo {
   nickname: string;
@@ -23,8 +25,7 @@ const EditInfo = () => {
   const handlePhoneChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setEdittedInfo({ ...edittedInfo, phone: e.target.value });
   };
-  const handleSave: FormEventHandler = (e) => {
-    e.preventDefault();
+  const handleSave = () => {
     if (!edittedInfo.nickname) {
       alert('닉네임을 입력해주세요');
       return;
@@ -34,73 +35,44 @@ const EditInfo = () => {
     console.log(edittedInfo);
   };
 
-  const inputStyle =
-    'w-full h-32 border-2 border-gray-300 rounded-9 px-4 py-2 focus:outline-none focus:border-black-modal';
-  const buttonStyle = 'w-full h-fit bg-black-modal text-white rounded-9';
-
   return (
     <div className="p-16 flex flex-col gap-12 w-767">
       <form className="text-16 flex flex-col gap-24" onSubmit={handleSave}>
-        <div className="flex flex-row gap-6 items-center">
+        <div className="flex flex-row gap-24 items-center">
           <label htmlFor="profile" className="p-40 rounded-9 bg-black-modal">
             프로필 사진
             <input type="file" id="profile" hidden />
           </label>
-          <div className="flex flex-col gap-6">
-            <label htmlFor="nickname">
-              닉네임
-              <input
-                type="text"
-                id="nickname"
-                className={inputStyle}
-                value={edittedInfo.nickname}
-                onChange={handleNicknameChange}
-                placeholder="닉네임을 입력해주세요"
-              />
-            </label>
-            <label htmlFor="email">
-              이메일
-              <input
-                type="text"
-                id="email"
-                className="w-full h-32 border-2 border-gray-300 rounded-9 px-4 py-2 text-gray-500 focus:outline-none"
-                value={edittedInfo.email}
-                disabled
-                placeholder="이메일을 입력해주세요"
-              />
-            </label>
+          <div className="flex flex-col gap-12">
+            <InputBox
+              label="닉네임"
+              value={edittedInfo.nickname}
+              onChange={handleNicknameChange}
+              placeholder="닉네임을 입력해주세요"
+              width="100%"
+            />
+            <InputBox label="이메일" value={edittedInfo.email} width="100%" />
           </div>
         </div>
         <div className="flex flex-col gap-12 w-fit">
-          <label htmlFor="name">
-            이름
-            <input
-              type="text"
-              id="name"
-              className={inputStyle}
-              value={edittedInfo.name}
-              onChange={handleNameChange}
-              placeholder="이름을 추가해주세요"
-            />
-          </label>
-          <label htmlFor="phone">
-            전화번호
-            <input
-              type="text"
-              id="phone"
-              className={inputStyle}
-              value={edittedInfo.phone}
-              onChange={handlePhoneChange}
-              placeholder="연락처를 추가해주세요"
-            />
-          </label>
-          <button type="button" className={buttonStyle}>
-            비밀번호 변경하기
-          </button>
+          <InputBox
+            label="이름"
+            value={edittedInfo.name}
+            onChange={handleNameChange}
+            placeholder="이름을 입력해주세요"
+          />
+          <InputBox
+            label="전화번호"
+            value={edittedInfo.phone}
+            onChange={handlePhoneChange}
+            placeholder="전화번호를 입력해주세요"
+          />
+          <Button
+            text="비밀번호 변경하기"
+            onClick={() => console.log('비밀번호 변경')}
+          />
         </div>
-        <button type="submit" className={buttonStyle} onSubmit={handleSave}>
-          저장
-        </button>
+        <Button text="저장하기" onClick={handleSave} />
       </form>
     </div>
   );
