5173af86aff8955d43fbc61593070012543aae6e
Hooni07 <gkxogns21@gmail.com>
2024-05-31 22:20:08 +0900

    Feat: 입력 상황에 따른 error 케이스 추가(react-hook-form)

--- src/pages/auth/signup/PartnerSignup.tsx
+++ src/pages/auth/signup/PartnerSignup.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 // import { useState } from 'react';
-import { EMAIL_REGEX } from '@/constants/InputType';
+import { EMAIL_REGEX, PASSWORD_REGEX } from '@/constants/InputType';
 import Logo from '@/assets/icons/travelPortLogo.svg';
 import Button from '@/components/common/Button';
 import InputBox from '@/components/common/InputBox';
@@ -24,6 +24,7 @@ const PartnerSignup = () => {
   const {
     register,
     handleSubmit,
+    getValues,
     formState: { errors },
   } = useForm<PartnerSignupForm>({
     mode: 'onChange',
@@ -62,7 +63,18 @@ const PartnerSignup = () => {
             className="flex flex-col gap-30 max-w-350 "
             onSubmit={handleSubmit(handleSignupForm)}
           >
-            <InputBox label="닉네임" width="35rem" placeholder="닉네임" />
+            <InputBox
+              label="이름/법인명"
+              width="35rem"
+              placeholder="이름/법인명"
+              error={errors.company}
+              register={register('company', {
+                required: {
+                  value: true,
+                  message: '이름 또는 법인명을 입력해주세요.',
+                },
+              })}
+            />
             <div className="flex gap-10">
               <div className="relative">
                 <InputBox
@@ -71,6 +83,10 @@ const PartnerSignup = () => {
                   placeholder="example@example.com"
                   error={errors.email}
                   register={register('email', {
+                    required: {
+                      value: true,
+                      message: '이메일을 입력해주세요.',
+                    },
                     pattern: {
                       value: EMAIL_REGEX,
                       message: '이메일 형식이 맞나요?',
@@ -103,12 +119,33 @@ const PartnerSignup = () => {
               inputType="password"
               width="350px"
               placeholder="비밀번호"
+              error={errors.password}
+              register={register('password', {
+                required: {
+                  value: true,
+                  message: '비밀번호를 입력해주세요.',
+                },
+                pattern: {
+                  value: PASSWORD_REGEX,
+                  message: '비밀번호 형식이 맞나요?',
+                },
+              })}
             />
             <InputBox
               label="비밀번호 확인"
               inputType="password"
-              width="350px"
+              width="35rem"
               placeholder="비밀번호 확인"
+              error={errors.passwordCheck}
+              register={register('passwordCheck', {
+                required: {
+                  value: true,
+                  message: '비밀번호 확인을 위해 입력해주세요.',
+                },
+                validate: (value) =>
+                  value === getValues('password') ||
+                  `위 비밀번호랑 일치하지 않아요!`,
+              })}
             />
             <Button
               text="회원가입 하기"
--- src/pages/auth/signup/UserSignup.tsx
+++ src/pages/auth/signup/UserSignup.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 // import { useState } from 'react';
-import { EMAIL_REGEX } from '@/constants/InputType';
+import { EMAIL_REGEX, PASSWORD_REGEX } from '@/constants/InputType';
 import Logo from '@/assets/icons/travelPortLogo.svg';
 import Button from '@/components/common/Button';
 import InputBox from '@/components/common/InputBox';
@@ -24,8 +24,12 @@ const UserSignup = () => {
   const {
     register,
     handleSubmit,
+    getValues,
     formState: { errors },
-  } = useForm<UserSignupForm>({ mode: 'onChange' });
+  } = useForm<UserSignupForm>({
+    mode: 'onChange',
+    defaultValues: { nickname: '', email: '', password: '', passwordCheck: '' },
+  });
   // const [isEmailValid, setIsEmailValid] = useState(false);
   const checkBtnBasic = 'absolute px-8 py-4 text-13 rounded top-44 right-12';
 
@@ -37,19 +41,15 @@ const UserSignup = () => {
     checkBtnClass = `${checkBtnBasic} bg-blue-1 text-black-13`;
   }
 
-  const signupClick = () => {
-    console.log('일반 회원가입');
-  };
-  // const checkClick = () => {
-  //   console.log('중복 확인');
-  // };
-
   const handleCheckEmail = (data: EmailForm) => {
     const { email } = data;
     console.log(email);
   };
 
-  const handleSignupForm = () => {};
+  const handleSignupForm = (data: UserSignupData) => {
+    const { nickname, email, password } = data;
+    console.log({ nickname, email, password });
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -62,7 +62,19 @@ const UserSignup = () => {
             className="flex flex-col gap-30 max-w-[35rem] "
             onSubmit={handleSubmit(handleSignupForm)}
           >
-            <InputBox label="닉네임" width="350px" placeholder="닉네임" />
+            <InputBox
+              label="닉네임"
+              width="350px"
+              placeholder="닉네임"
+              error={errors.nickname}
+              register={register('nickname', {
+                required: { value: true, message: '닉네임을 입력해주세요.' },
+                maxLength: {
+                  value: 20,
+                  message: '닉네임은 20자 이하로 입력해주세요',
+                },
+              })}
+            />
             <div className="flex gap-10">
               <div className="relative">
                 <InputBox
@@ -71,6 +83,10 @@ const UserSignup = () => {
                   placeholder="example@example.com"
                   error={errors.email}
                   register={register('email', {
+                    required: {
+                      value: true,
+                      message: '이메일을 입력해주세요.',
+                    },
                     pattern: {
                       value: EMAIL_REGEX,
                       message: '이메일 형식이 맞나요?',
@@ -103,14 +119,38 @@ const UserSignup = () => {
               inputType="password"
               width="350px"
               placeholder="비밀번호"
+              error={errors.password}
+              register={register('password', {
+                required: {
+                  value: true,
+                  message: '비밀번호를 입력해주세요.',
+                },
+                pattern: {
+                  value: PASSWORD_REGEX,
+                  message: '비밀번호 형식이 맞나요?',
+                },
+              })}
             />
             <InputBox
               label="비밀번호 확인"
               inputType="password"
               width="350px"
               placeholder="비밀번호 확인"
+              error={errors.passwordCheck}
+              register={register('passwordCheck', {
+                required: {
+                  value: true,
+                  message: '비밀번호 확인을 위해 입력해주세요.',
+                },
+                validate: (value) =>
+                  value === getValues('password') ||
+                  `위 비밀번호랑 일치하지 않아요!`,
+              })}
             />
-            <Button text="회원가입 하기" onClick={signupClick} />
+            <Button
+              text="회원가입 하기"
+              onClick={handleSubmit(handleSignupForm)}
+            />
           </form>
 
           <div className="text-center text-14 text-black-13">
