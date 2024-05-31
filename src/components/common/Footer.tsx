33466cb23cb5f8e4b0897e90dfd115878be43a55
박지윤 <tkrhkfld123@naver.com>
2024-05-31 15:51:00 +0900

    style: footer 반응형 구현 완료

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,31 +5,33 @@ import insta from '@/assets/icons/instagram.svg';
 
 const Footer = () => {
   return (
-    <footer className="mobile:flex-col mobile:gap-[0.8rem] tablet:gap-[12rem] gap-[30rem] mt-[2rem] bg-[#EBF1FF] flex flex-row items-center justify-center py-[2rem] px-[4rem] space-y-4 text-center">
-      <div>
-        <div>©TravelPort - 2024</div>
-      </div>
-
-      <div className="flex tablet:gap-[12rem] gap-[30rem] mobile:gap-[4.4rem]">
-        <div className="flex space-x-4">
-          <a href="/" className="text-[#A4A1AA] px-4 py-3">
-            Privacy Policy
-          </a>
-          <a href="/" className="text-[#A4A1AA] px-4 py-3">
-            FAQ
-          </a>
+    <footer className="w-full mt-[2rem] bg-[#EBF1FF] flex flex-col mobile:flex-col justify-center items-center py-[2rem] px-[4rem] text-center">
+      <div className="flex mobile:flex-col flex-row tablet:gap-[12rem] gap-[30rem] mobile:gap-[0.8rem] items-center">
+        <div>
+          <div>©TravelPort - 2024</div>
         </div>
 
-        <div className="flex space-x-4 gap-[2rem] mobile:gap-[0.8rem]">
-          <a aria-label="Save" href="/">
-            <img src={message} alt="메세지 아이콘" />
-          </a>
-          <a aria-label="Save" href="/">
-            <img src={facebook} alt="페이스북 아이콘" />
-          </a>
-          <a aria-label="Save" href="/">
-            <img src={insta} alt="인스타그램 아이콘" />
-          </a>
+        <div className="flex flex-row tablet:gap-[12rem] gap-[30rem] mobile:gap-[4.4rem] items-center">
+          <div className="ㅎ메 space-x-4 mobile:flex-col tablet:flex-row mobile:space-x-0">
+            <a href="/" className="text-[#A4A1AA] px-4 py-3">
+              Privacy Policy
+            </a>
+            <a href="/" className="text-[#A4A1AA] px-4 py-3">
+              FAQ
+            </a>
+          </div>
+
+          <div className="flex flex-row space-x-4 mobile:space-x-0 gap-[2rem] mobile:gap-[0.8rem] items-center">
+            <a aria-label="Save" href="/">
+              <img src={message} alt="메세지 아이콘" />
+            </a>
+            <a aria-label="Save" href="/">
+              <img src={facebook} alt="페이스북 아이콘" />
+            </a>
+            <a aria-label="Save" href="/">
+              <img src={insta} alt="인스타그램 아이콘" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
