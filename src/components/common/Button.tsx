5a369c327349dab6d065a635a8c4b8f80f6d3eee
Hooni07 <gkxogns21@gmail.com>
2024-05-31 16:18:27 +0900

    Feat: Button 변경 디자인 및 동작 적용

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,16 +1,34 @@
 type ButtonProps = {
+  variant?: 'default' | 'cancel';
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-const Button = ({ text = '', onClick }: ButtonProps) => {
+const Button = ({
+  variant = 'default',
+  text = '',
+  onClick,
+  disabled = false,
+}: ButtonProps) => {
+  const buttonBasic = 'flex justify-center items-center p-10 rounded text-16';
+  const disabledDesign = 'bg-black-5 text-black-7';
+  const cancelDesign = 'bg-black-5 text-black-13';
+
+  let buttonClass = `${buttonBasic} bg-blue-6 hover:bg-blue-5 active:bg-blue-7 text-white`;
+
+  if (disabled) {
+    buttonClass = `${buttonBasic} ${disabledDesign}`;
+  } else if (variant === 'cancel') {
+    buttonClass = `${buttonBasic} ${cancelDesign}`;
+  }
+
   return (
     <button
-      className="flex justify-center items-center px-15 py-10
-    border-1 rounded-md border-solid border-blue-700 
-    text-3xl"
+      className={buttonClass}
       type="button"
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
