0b448ea463e703f04be8658959b15eca8fd82294
eugene9851 <eugenelee980713@gmail.com>
2024-05-31 19:35:11 +0900

    Feat: 별점 클릭 시 데이터 즉시 반영되도록 구현

--- src/components/review/ReviewStar.tsx
+++ src/components/review/ReviewStar.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { useEffect, useState } from 'react';
 import { ImStarEmpty, ImStarFull } from 'react-icons/im';
 
-const ReviewStar = () => {
+type ReviewStarProps = {
+  onChange: (selectedScore: number) => void;
+};
+const ReviewStar = ({ onChange }: ReviewStarProps) => {
   const [clicked, setClicked] = useState([false, false, false, false, false]);
 
   const array = [0, 1, 2, 3, 4];
+
   const handleStarClick = (index: number) => {
     const clickStates = [...clicked];
     for (let i = 0; i < 5; i++) {
@@ -13,10 +18,12 @@ const ReviewStar = () => {
     setClicked(clickStates);
   };
 
-  const score = clicked.filter(
-    (clickedBoolean) => clickedBoolean === true,
-  ).length;
-  console.log(score); // score post하기
+  useEffect(() => {
+    const score = clicked.filter(
+      (clickedBoolean) => clickedBoolean === true,
+    ).length;
+    onChange(score);
+  }, [clicked, onChange]);
 
   return (
     <div className="flex">
