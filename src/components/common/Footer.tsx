593af163fe3c478d2cb5069f282418625afe860e
박지윤 <tkrhkfld123@naver.com>
2024-05-31 12:29:43 +0900

    style: 반응형 구현 완료

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,27 +5,22 @@ import insta from '@/assets/icons/instagram.svg';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#EBF1FF] h-[6rem] flex items-center">
-      <div className="md:gap-[12rem] xl:gap-[30rem] flex items-center px-16 md:px-8 py-8 justify-between max-w-screen-xl mx-auto text-[1.5rem] w-full ">
-        <div className="font-medium text-[#7D7986] text-right">
-          ©TravelPort - 2024
-        </div>
+    <footer className="mobile:flex-col mobile:gap-[0.8rem] tablet:gap-[12rem] gap-[30rem] mt-[2rem] bg-[#EBF1FF] flex flex-row items-center justify-center py-[2rem] px-[4rem] space-y-4 text-center">
+      <div>
+        <div>©TravelPort - 2024</div>
+      </div>
 
-        <div
-          className="flex font-normal gap-[1.2rem]"
-          style={{
-            margin: '0 auto',
-          }}
-        >
-          <a href="/" className="text-[#A4A1AA] p-[1.2rem]">
+      <div className="flex tablet:gap-[12rem] gap-[30rem] mobile:gap-[4.4rem]">
+        <div className="flex space-x-4">
+          <a href="/" className="text-[#A4A1AA] px-4 py-3">
             Privacy Policy
           </a>
-          <a href="/" className="text-[#A4A1AA] p-[1.2rem]">
+          <a href="/" className="text-[#A4A1AA] px-4 py-3">
             FAQ
           </a>
         </div>
 
-        <div className="flex gap-[1.2rem] mt-[-0.8rem] justify-start">
+        <div className="flex space-x-4 gap-[2rem] mobile:gap-[0.8rem]">
           <a aria-label="Save" href="/">
             <img src={message} alt="메세지 아이콘" />
           </a>
