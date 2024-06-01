c8bdf8a275f29318dfcc3984fe588053588b7ef7
Hooni07 <gkxogns21@gmail.com>
2024-06-01 19:21:10 +0900

    Feat: cookie 관련 기능 함수 생성 및 CookieProvider 적용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CookiesProvider } from 'react-cookie';
 import App from './App';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </React.StrictMode>,
 );
