da78e738091256636b6cf105fe2c0ce601cd2ba7
byunjiin <jibyun5808@naver.com>
2024-06-01 20:11:24 +0900

    Feat: Card 반응형 구현

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -22,17 +22,23 @@ const Card = () => {
   };
 
   return (
-    <div className=" w-265 h-406 overflow-hidden rounded-xl shadow-[0_0_4px_0_rgba(0,0,0,0.25)]">
-      <img src={image} alt="예시사진" className="w-265 h-267 object-cover" />
-      <div className="w-265 h-139 bg-white px-16 py-20 hover:bg-blue-1">
+    <div className="flex flex-col w-265 h-400 overflow-hidden rounded-xl shadow-[0_0_4px_0_rgba(0,0,0,0.25)] tablet:w-229 tablet:h-350 mobile:w-335 mobile:h-198 mobile:flex-row">
+      <img
+        src={image}
+        alt="예시사진"
+        className="w-265 h-267 object-cover tablet:w-229 tablet:h-231 mobile:w-158 mobile:h-198"
+      />
+      <div className="w-265 h-133 bg-white px-16 py-20 hover:bg-blue-1 tablet:w-229 tablet:h-119 mobile:w-177 mobile:h-198">
         <p className="text-black-13 pb-5 text-xl font-semibold leading-7">
           {title}
         </p>
         <p className="text-black-13 pb-8 text-xs leading-4">{location}</p>
-        <div className="flex items-center gap-8 pb-16">
-          <div className="flex w-75">{imageScore()}</div>
-          <p className="text-black-7 text-[11px] leading-3">( {score} / 5 )</p>
-          <p className="text-black-7 text-[11px] leading-3">
+        <div className="flex flex-row items-center pb-16 mobile:flex-col mobile:items-start ">
+          <div className="flex flex-row pr-8 mobile:pb-8">
+            <div className="flex w-75">{imageScore()}</div>
+            <p className="text-black-7 text-xs pl-4">( {score} / 5 )</p>
+          </div>
+          <p className="text-black-7 text-xs leading-3">
             리뷰 {review.toLocaleString()}개
           </p>
         </div>
