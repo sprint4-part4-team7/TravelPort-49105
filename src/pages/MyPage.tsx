0ba891c596e62f454588fca9a516615ae1b4e5f9
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-02 19:33:34 +0900

    Feature: 예약 관리 컴포넌트 임시 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,17 +2,20 @@ import { useUserMypageStore } from '@/utils/zustand';
 import EditInfo from '@/components/myPage/EditUserInfo';
 import EditPartnerInfo from '@/components/myPage/EditPartnerInfo';
 import MyPageSideBar from '@/components/myPage/MyPageSideBar';
+import MyResevation from '@/components/myPage/MyResevation';
+import Layout from '@/components/common/layout/Layout';
 
 const MyPage = () => {
   const { userMypage } = useUserMypageStore();
 
   return (
-    <div>
+    <Layout userType="user">
       <MyPageSideBar>
         {userMypage === 'user' && <EditInfo />}
         {userMypage === 'partner' && <EditPartnerInfo />}
+        {userMypage === 'reservation' && <MyResevation />}
       </MyPageSideBar>
-    </div>
+    </Layout>
   );
 };
 
