4979a57ffc97d9b4b67732ed708515c7a62a0204
byunjiin <jibyun5808@naver.com>
2024-06-03 01:32:26 +0900

    Style: 바귄 폰트에 따라 변경

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -39,23 +39,23 @@ const Card = ({
 
   return (
     <Link to={link}>
-      <div className="flex flex-col w-265 h-400 overflow-hidden rounded-xl shadow-[0_0_4px_0_rgba(0,0,0,0.25)] tablet:w-229 tablet:h-350 mobile:w-335 mobile:h-198 mobile:flex-row">
+      <div className="flex flex-col w-265 h-400 overflow-hidden rounded-xl shadow-[0_0_4px_0_rgba(0,0,0,0.25)] font-plexSans tablet:w-229 tablet:h-350 mobile:w-335 mobile:h-198 mobile:flex-row">
         <img
           src={image}
           alt="예시사진"
           className="w-265 h-267 object-cover tablet:w-229 tablet:h-231 mobile:w-158 mobile:h-198"
         />
         <div className="w-265 h-133 bg-white px-16 py-20 hover:bg-blue-1 tablet:w-229 tablet:h-119 mobile:w-177 mobile:h-198">
-          <p className="text-black-12 pb-5 text-xl font-semibold leading-7">
+          <h2 className="text-black-12 mb-5 text-20 py-4 h-28 font-semibold leading-7">
             {title}
-          </p>
-          <p className="text-black-12 pb-8 text-xs leading-4">{location}</p>
-          <div className="flex flex-row items-center pb-16 mobile:flex-col mobile:items-start ">
-            <div className="flex flex-row pr-8 mobile:pb-8">
-              <div className="flex w-75">{ImageScore()}</div>
-              <p className="text-black-6 text-xs pl-4">( {score} / 5 )</p>
+          </h2>
+          <p className="text-black-12 mb-8 text-12 leading-4">{location}</p>
+          <div className="flex flex-row items-center mb-16 mobile:flex-col mobile:items-start ">
+            <div className="flex flex-row item-center pr-8 mobile:mb-8">
+              <div className="flex flex-row w-85">{ImageScore()}</div>
+              <p className="text-black-6 text-11 pl-4">( {score} / 5 )</p>
             </div>
-            <p className="text-black-6 text-xs leading-3">
+            <p className="text-black-6 text-11 leading-3">
               리뷰 {review.toLocaleString()}개
             </p>
           </div>
