b4fe55b4f032e2ca33592cb9922fb40e4a2b2bd3
박지윤 <tkrhkfld123@naver.com>
2024-06-03 12:41:03 +0900

    chore: 리액트 쿼리 세팅

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './styles/global.css';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </React.StrictMode>
+  </QueryClientProvider>,
 );
