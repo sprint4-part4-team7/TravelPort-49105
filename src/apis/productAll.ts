b46cffec670014653f2100ff2d89dfbf95db5722
byunjiin <jibyun5808@naver.com>
2024-06-04 15:26:28 +0900

    Feat: 전체 상품조회용 api

---
+++ src/apis/productAll.ts
@@ -0,0 +1,24 @@
+// 상품 전체의 api
+import instance from '@/utils/axios';
+
+export const getProductAll = async () => {
+  try {
+    const response = await instance.get(`/product/all`);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getProductOptionAll = async () => {
+  try {
+    const response = await instance.get(`/productOption/all`);
+    console.log(response.data);
+    return response.data.productOption;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
