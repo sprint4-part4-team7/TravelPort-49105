3a675a78c4bd6e18fdb74756e542aa54b45a2df9
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-04 19:49:11 +0900

    Style: 마이페이지 사용자 정보 수정 디자인 적용

--- src/components/myPage/EditUserInfo.tsx
+++ src/components/myPage/EditUserInfo.tsx
@@ -1,6 +1,7 @@
 import { useUserStore } from '@/utils/zustand';
 import { useForm } from 'react-hook-form';
 import useModal from '@/hooks/useModal';
+import uploadIcon from '@/assets/icons/upload.svg';
 import Button from '@/components/common/Button';
 import InputBox from '../common/InputBox';
 import Modal from '../common/Modal';
@@ -12,6 +13,7 @@ interface UserInfo {
   email: string;
   name?: string;
   phone?: string;
+  profileImage?: string;
 }
 
 const EditInfo = () => {
@@ -22,7 +24,7 @@ const EditInfo = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<UserInfo>({
-    defaultValues: userInfo,
+    defaultValues: { ...userInfo },
   });
 
   const { isModalOpen, openModal, closeModal } = useModal();
@@ -32,50 +34,55 @@ const EditInfo = () => {
     alert('저장되었습니다');
     console.log(data);
   };
-  console.log(userInfo);
 
   return (
-    <div className="p-16 flex flex-col gap-12 w-767">
+    <div className="flex flex-col gap-12 p-24 w-full">
       <form
         className="text-16 flex flex-col gap-24"
         onSubmit={handleSubmit(handleSave)}
       >
         <div className="flex flex-row gap-24 items-center">
-          <label
-            htmlFor="profile"
-            className="relative w-120 h-120 rounded-9 bg-black-7"
-          >
-            {userInfo?.profileImage ? (
-              <img
-                src={userInfo.profileImage}
-                className="rounded-9"
-                alt="profile"
-              />
-            ) : (
-              <div className="absolute top-0">프로필</div>
-            )}
-
-            <input type="file" id="profile" hidden />
-          </label>
-          <div className="flex flex-col gap-12">
-            <InputBox
-              label="닉네임"
-              placeholder="닉네임을 입력해주세요"
-              register={register('nickname', {
-                required: '닉네임은 필수입니다',
-                maxLength: {
-                  value: 20,
-                  message: '닉네임은 20자 이하로 입력해주세요',
-                },
-              })}
+          {userInfo?.profileImage ? (
+            <img
+              src={userInfo.profileImage}
+              className="rounded-full w-140 h-140 object-cover"
+              alt="profile"
             />
-            <div className="text-system-error text-12">
-              {errors.nickname?.message && `${errors.nickname.message}`}
-            </div>
-            <InputBox label="이메일" register={register('email')} disabled />
+          ) : (
+            <div className="w-140 h-140 rounded-full bg-black-6" />
+          )}
+          <div className="flex flex-col gap-12">
+            <label
+              htmlFor="profileImg"
+              className="flex flex-row gap-8 rounded-30 w-fit border-1 border-blue-6 p-16 text-16 text-blue-6 font-normal"
+            >
+              <img src={uploadIcon} alt="upload" />
+              프로필 사진 업로드
+              <input
+                id="profileImg"
+                hidden
+                type="file"
+                accept="image/*"
+                {...register('profileImage')}
+              />
+            </label>
+            <div>최대 10mb까지 업로드 가능합니다.</div>
           </div>
         </div>
-        <div className="flex flex-col gap-12 w-fit">
+        <div className="flex flex-col gap-32 w-full">
+          <InputBox
+            label="닉네임"
+            placeholder="닉네임을 입력해주세요"
+            register={register('nickname', {
+              required: '닉네임은 필수입니다',
+              maxLength: {
+                value: 20,
+                message: '닉네임은 20자 이하로 입력해주세요',
+              },
+            })}
+            error={errors.nickname}
+          />
+          <InputBox label="이메일" register={register('email')} disabled />
           <InputBox
             label="이름"
             placeholder="이름을 입력해주세요"
@@ -85,10 +92,8 @@ const EditInfo = () => {
                 message: '이름은 20자 이하로 입력해주세요',
               },
             })}
+            error={errors.name}
           />
-          <div className="text-system-error text-12">
-            {errors.name?.message && `${errors.name.message}`}
-          </div>
           <InputBox
             label="전화번호"
             placeholder="'-' 없이 입력해주세요"
@@ -98,14 +103,26 @@ const EditInfo = () => {
                 message: '전화번호 형식에 맞게 입력해주세요',
               },
             })}
+            error={errors.phone}
           />
-          <div className="text-system-error text-12">
-            {errors.phone?.message && `${errors.phone.message}`}
+          <div>
+            <InputBox
+              inputType="password"
+              label="비밀번호"
+              placeholder="********"
+              disabled
+            />
+            <button
+              type="button"
+              className="p-12 text-16 text-blue-6 font-normal flex flex-row gap-4"
+              onClick={openModal}
+            >
+              비밀번호 변경하기
+            </button>
           </div>
-          <Button text="비밀번호 변경하기" onClick={openModal} />
         </div>
-        <Button text="저장하기" onClick={handleSubmit(handleSave)} />
       </form>
+      <Button text="저장하기" onClick={handleSubmit(handleSave)} />
       <Modal isOpen={isModalOpen} closeModal={closeModal}>
         <ChangePassword closeModal={closeModal} />
       </Modal>
