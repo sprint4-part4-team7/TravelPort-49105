26b7af23184eee6b274d802ff3a89729b43d13e1
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-04 18:29:53 +0900

    Style: 마이페이지 사이드바 디자인 적용

--- src/components/myPage/MyPageSideBar.tsx
+++ src/components/myPage/MyPageSideBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useUserMypageStore } from '@/utils/zustand';
+import { useUserMypageStore, useUserStore } from '@/utils/zustand';
 import Button from '../common/Button';
+import MyPageButton from './MyPageButton';
 
 interface MyPageSideBarProps {
   children: React.ReactNode;
@@ -8,7 +9,7 @@ interface MyPageSideBarProps {
 
 const MyPageSideBar = ({ children }: MyPageSideBarProps) => {
   const setMypage = useUserMypageStore((state) => state.setUserMypage);
-
+  const { userInfo } = useUserStore();
   const handleStorageClear = () => {
     localStorage.clear();
     window.location.reload();
@@ -16,11 +17,38 @@ const MyPageSideBar = ({ children }: MyPageSideBarProps) => {
 
   return (
     <div className="flex flex-row gap-24 w-full">
-      <div className="flex flex-col w-144 gap-12 p-12">
-        <Button text="사용자 정보 수정" onClick={() => setMypage('user')} />
-        <Button text="파트너 정보 수정" onClick={() => setMypage('partner')} />
-        <Button text="예약 내역" onClick={() => setMypage('reservation')} />
-        <Button text="스토리지 정리" onClick={handleStorageClear} />
+      <div className="flex flex-col relative justify-between px-12 border-r-1 border-black-4">
+        <div className="flex flex-col w-241 gap-32">
+          <div className="flex flex-col gap-12 items-center">
+            {userInfo?.profileImage ? (
+              <img
+                src={userInfo.profileImage}
+                alt="profile"
+                className="w-120 h-120 rounded-full"
+              />
+            ) : null}
+            <span className="text-14 font-semibold px-4 py-8">
+              {userInfo.name || '사용자 이름'}
+            </span>
+          </div>
+          <div className="flex flex-col gap-4">
+            <MyPageButton setMyPage={setMypage} page="user">
+              정보 수정
+            </MyPageButton>
+            <MyPageButton setMyPage={setMypage} page="partner">
+              파트너 정보 수정
+            </MyPageButton>
+            <MyPageButton setMyPage={setMypage} page="reservation">
+              예약 내역
+            </MyPageButton>
+          </div>
+        </div>
+        <Button
+          text="로그아웃"
+          isCancel
+          outlined
+          onClick={handleStorageClear}
+        />
       </div>
       {children}
     </div>
