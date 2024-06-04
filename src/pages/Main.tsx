7759d267083ce77acde196d354ff507ed510cb3f
박지윤 <tkrhkfld123@naver.com>
2024-06-04 13:51:51 +0900

    chore: gap수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -53,43 +53,6 @@ const images: ImageItem[] = [
     score: 2,
     review: 103,
   },
-  {
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKD1-YGiJrzk3PakRGE8AbqtzgkEG3iWpoA&usqp=CAU',
-    text: '낭만의 액티비티, 열기구',
-    path: '/',
-    location: '인천 남동구',
-    price: 30000,
-    score: 5,
-    review: 1034,
-  },
-
-  {
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU',
-    text: '잔잔한 호수위로, 동동배',
-    path: '/',
-    location: '서울 마포구',
-    price: 50000,
-    score: 5,
-    review: 103,
-  },
-  {
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZJh0dep1ggbJRjae6T8ux_lDc3n2ZiW55bw&usqp=CAU',
-    text: '빛나는 도심 속, 별 구경하기',
-    path: '/',
-    location: '서울 마포구',
-    price: 40000,
-    score: 3,
-    review: 112,
-  },
-  {
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm5UHoR3EZQ9Yu0Cs-SVxI4Yx5Y1uBtCrqbg&usqp=CAU',
-    text: '나른한 오후 1시, 호수 구경하기',
-    path: '/',
-    location: '인천 미추홀구',
-    price: 30000,
-    score: 2,
-    review: 103,
-  },
 ];
 
 const Main = () => {
@@ -98,7 +61,7 @@ const Main = () => {
       <Layout userType="user" main>
         <Carousel items={images} />
         <div className="flex justify-center p-20">
-          <div className="grid gap-4 mobile:grid-cols-1 tablet:grid-cols-3 desktop:grid-cols-4">
+          <div className="grid gap-20 mobile:grid-cols-1 tablet:grid-cols-3 desktop:grid-cols-4">
             {images.map((item, index) => (
               <Card
                 key={index}
