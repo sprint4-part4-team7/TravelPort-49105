17bf49058a3ff2088eb38fca51d271dea5e1e290
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-04 22:57:25 +0900

    Refactor: 마이페이지 예약 목록 칩&버튼 구현 및 예약 페이지네이션 리팩토링

--- src/components/myPage/ResevationCard.tsx
+++ src/components/myPage/ResevationCard.tsx
@@ -1,29 +1,40 @@
+import { ReactNode } from 'react';
+// import ReservButton from './ReservButton';
+// import ReservChips from './ReservChips';
+
 interface ReservationCardProps {
-  reservation: {
-    id: number;
-    title: string;
-    date: string;
-    option: string;
-    status: string;
-  };
+  id: number;
+  title: string;
+  date: string;
+  option?: string;
+  upperRight?: ReactNode;
+  lowerRight?: ReactNode;
 }
 
-const ReservationCard = ({ reservation }: ReservationCardProps) => {
+const ReservationCard = ({
+  id,
+  title,
+  date,
+  option,
+  upperRight,
+  lowerRight,
+}: ReservationCardProps) => {
   return (
-    <div className="flex flex-col p-16 w-full max-w-834 gap-32 border-1 border-black-5 rounded-8">
+    <div
+      id={id.toString()}
+      className="flex flex-col p-16 w-full max-w-834 gap-32 border-1 border-black-5 rounded-8"
+    >
       <div className="flex flex-col justify-between gap-32">
         <div className="flex flex-col gap-12">
           <div className="flex flex-row font-semibold justify-between">
-            <div className="text-20">{reservation.title}</div>
-            <div className="text-14 px-8 py-4">{reservation.status}</div>
+            <div className="text-20">{title}</div>
+            {upperRight}
           </div>
-          <div className="text-16">{reservation.option}</div>
+          <div className="text-16">{option}</div>
         </div>
         <div className="flex flex-row justify-between items-center">
-          <div className="text-16">{reservation.date}</div>
-          <div className="flex flex-row px-12 py-8 gap-4 text-14 font-semibold">
-            후기 쓰기
-          </div>
+          <div className="text-16">{date}</div>
+          {lowerRight}
         </div>
       </div>
     </div>
