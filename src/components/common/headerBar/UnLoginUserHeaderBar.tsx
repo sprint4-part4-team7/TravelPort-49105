59e26e0e64c039ef76a05d0ec278fd849fc97f56
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-06-04 16:57:07 +0900

    Fix: 공용 Button 컴포넌트 적용 및 rem 단위 변경(통일성 유지)

--- src/components/common/headerBar/UnLoginUserHeaderBar.tsx
+++ src/components/common/headerBar/UnLoginUserHeaderBar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import menu from '@/assets/icons/menu.svg';
+import Button from '@/components/common/Button';
 
 const UnLoginUserHeaderBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,51 +13,50 @@ const UnLoginUserHeaderBar = () => {
       {/* 모바일 환경에서만 햄버거 아이콘 표시 */}
       <div className="hidden mobile:flex">
         <button type="button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          <img className="h-[3.2rem] w-[3.2rem]" src={menu} alt="Menu" />
+          <img className="h-32 w-32" src={menu} alt="Menu" />
         </button>
       </div>
 
       {/* 드롭다운 메뉴 */}
       {isMenuOpen && (
-        <div className="absolute right-[-5rem] z-50 flex-col items-start justify-start hidden p-4 bg-white w-[15rem] top-full mobile:flex rounded-5">
-          <button
-            type="button"
-            className="box-border border-1 border-[#356EFF] border-solid p-[0.75rem] rounded-4 text-[#356EFF] text-[1.4rem] font-semibold leading-[142%] mb-4 w-full text-left"
+        <div className="absolute -right-50 z-50 flex-col gap-5 items-start justify-start hidden p-4 bg-white w-150 top-full mobile:flex rounded">
+          <Button
+            outlined
+            buttonStyle="text-14 p-8 font-semibold"
+            text="파트너 페이지"
             onClick={() => {
               navigate('./partner');
               setIsMenuOpen(false);
             }}
-          >
-            파트너 페이지
-          </button>
-          <button
-            type="button"
-            className="bg-[#356EFF] text-white p-[0.85rem] rounded-4 text-[1.4rem] font-semibold leading-[142%] w-full text-left"
+          />
+          <Button
+            buttonStyle="text-14 p-8 font-semibold"
+            text="로그인 및 회원가입"
             onClick={() => {
               navigate('./login');
               setIsMenuOpen(false);
             }}
-          >
-            로그인 및 회원가입
-          </button>
+          />
         </div>
       )}
 
       {/* 태블릿 및 PC 환경에서 회원가입 / 로그인 버튼들 표시 */}
-      <div className=" mobile:hidden space-x-[1.2rem]">
-        <button
-          type="button"
-          className="box-border border-1 border-[#356EFF] border-solid p-[0.75rem] rounded-4 text-[#356EFF] text-[1.4rem] font-semibold leading-[142%]"
-        >
-          파트너 페이지
-        </button>
-        <button
-          type="button"
-          className="bg-[#356EFF] text-white p-[0.85rem] rounded-4 text-[1.4rem] font-semibold leading-[142%]"
-          onClick={() => navigate('./login')}
-        >
-          로그인 및 회원가입
-        </button>
+      <div className="flex mobile:hidden space-x-12">
+        <Button
+          outlined
+          buttonStyle="min-w-fit text-14 p-10 font-semibold"
+          text="파트너 페이지"
+          onClick={() => {
+            navigate('./partner');
+          }}
+        />
+        <Button
+          buttonStyle="min-w-fit text-14 p-10 font-semibold"
+          text="로그인 및 회원가입"
+          onClick={() => {
+            navigate('./login');
+          }}
+        />
       </div>
     </div>
   );
