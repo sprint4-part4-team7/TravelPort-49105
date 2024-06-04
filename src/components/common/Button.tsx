fdb330b8b88f868ac7f79f20bc85684a409630ee
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-06-04 14:12:44 +0900

    Fix: 변경된 Button 컴포넌트 Props 반영

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,27 +1,34 @@
 type ButtonProps = {
-  variant?: 'default' | 'cancel';
+  variant?: 'default' | 'outlined' | 'more' | 'floating';
+  buttonStyle?: string; // font 굵기, text 크기, padding, width 변경 시
   text: string;
+  isCancel?: boolean;
   onClick?: () => void;
   disabled?: boolean;
 };
 
 const Button = ({
   variant = 'default',
+  buttonStyle = 'text-16 p-12 w-full',
   text = '',
+  isCancel = false,
   onClick,
   disabled = false,
 }: ButtonProps) => {
-  const buttonBasic =
-    'flex justify-center items-center p-10 rounded text-16 font-bold';
+  const basic = `flex justify-center items-center rounded ${buttonStyle}`;
+  const default = 'bg-blue-6 hover:bg-blue-5 active:bg-blue-7 text-white';
+  const outlined =
+    'bg-white border-1 border-solid border-blue-6 text-blue-6 hover:border-blue-5 hover:text-blue-5 active:border-blue-7 active:text-blue-7 ';
+
   const disabledDesign = 'bg-black-4 text-black-6';
   const cancelDesign = 'bg-black-4 text-black-12';
 
-  let buttonClass = `${buttonBasic} bg-blue-6 hover:bg-blue-5 active:bg-blue-7 text-white`;
+  let buttonClass = `${basic} bg-blue-6 hover:bg-blue-5 active:bg-blue-7 text-white`;
 
   if (disabled) {
-    buttonClass = `${buttonBasic} ${disabledDesign}`;
+    buttonClass = `${basic} ${disabledDesign}`;
   } else if (variant === 'cancel') {
-    buttonClass = `${buttonBasic} ${cancelDesign}`;
+    buttonClass = `${basic} ${cancelDesign}`;
   }
 
   return (
