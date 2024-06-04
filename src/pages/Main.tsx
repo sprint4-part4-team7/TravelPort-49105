8a3efcaa4c688ce45d7a75f41582a760860b9f90
박지윤 <tkrhkfld123@naver.com>
2024-06-04 13:35:44 +0900

    style: 카드 컴포넌트 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/no-array-index-key */
 import Carousel from '@/components/Carousel';
+import Card from '@/components/common/card/Card';
 import Footer from '@/components/common/Footer';
 import Layout from '@/components/common/layout/Layout';
 
@@ -9,6 +10,10 @@ interface ImageItem {
   url: string;
   text?: string;
   path?: string;
+  location?: string;
+  price?: number;
+  score: number;
+  review: number;
 }
 
 const images: ImageItem[] = [
@@ -16,22 +21,74 @@ const images: ImageItem[] = [
     url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKD1-YGiJrzk3PakRGE8AbqtzgkEG3iWpoA&usqp=CAU',
     text: '낭만의 액티비티, 열기구',
     path: '/',
+    location: '인천 남동구',
+    price: 30000,
+    score: 5,
+    review: 1034,
+  },
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU',
+    text: '잔잔한 호수위로, 동동배',
+    path: '/',
+    location: '서울 마포구',
+    price: 50000,
+    score: 5,
+    review: 103,
+  },
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZJh0dep1ggbJRjae6T8ux_lDc3n2ZiW55bw&usqp=CAU',
+    text: '빛나는 도심 속, 별 구경하기',
+    path: '/',
+    location: '서울 마포구',
+    price: 40000,
+    score: 3,
+    review: 112,
+  },
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm5UHoR3EZQ9Yu0Cs-SVxI4Yx5Y1uBtCrqbg&usqp=CAU',
+    text: '나른한 오후 1시, 호수 구경하기',
+    path: '/',
+    location: '인천 미추홀구',
+    price: 30000,
+    score: 2,
+    review: 103,
+  },
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKD1-YGiJrzk3PakRGE8AbqtzgkEG3iWpoA&usqp=CAU',
+    text: '낭만의 액티비티, 열기구',
+    path: '/',
+    location: '인천 남동구',
+    price: 30000,
+    score: 5,
+    review: 1034,
   },
 
   {
     url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYeU_xa0PN9zgmYzlkTMVGKJ4ulAGevTa9A&usqp=CAU',
     text: '잔잔한 호수위로, 동동배',
     path: '/',
+    location: '서울 마포구',
+    price: 50000,
+    score: 5,
+    review: 103,
   },
   {
     url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZJh0dep1ggbJRjae6T8ux_lDc3n2ZiW55bw&usqp=CAU',
     text: '빛나는 도심 속, 별 구경하기',
     path: '/',
+    location: '서울 마포구',
+    price: 40000,
+    score: 3,
+    review: 112,
   },
   {
     url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm5UHoR3EZQ9Yu0Cs-SVxI4Yx5Y1uBtCrqbg&usqp=CAU',
     text: '나른한 오후 1시, 호수 구경하기',
     path: '/',
+    location: '인천 미추홀구',
+    price: 30000,
+    score: 2,
+    review: 103,
   },
 ];
 
@@ -40,18 +97,21 @@ const Main = () => {
     <>
       <Layout userType="user" main>
         <Carousel items={images} />
-        <div className="font-plexSans mt-[8.2rem] grid grid-cols-4 gap-[2.4rem]">
-          {images.map((item, index) => (
-            <div key={index}>
-              <img
-                src={item.url}
-                alt={item.text}
-                className="w-[32.4rem] h-[24rem] rounded-[1.2rem]"
-                onClick={() => item.path && (window.location.href = item.path)}
-                style={{ cursor: 'pointer' }}
+        <div className="flex justify-center p-20">
+          <div className="grid gap-4 mobile:grid-cols-1 tablet:grid-cols-3 desktop:grid-cols-4">
+            {images.map((item, index) => (
+              <Card
+                key={index}
+                title={item.text as string}
+                location={item.location as string}
+                price={item.price as number}
+                score={item.score as number}
+                review={item.review as number}
+                image={item.url as string}
+                link={item.path as string}
               />
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </Layout>
       <Footer />
