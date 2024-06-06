caf71f6db02c133ba4438ed633c4305c59ddf1da
박지윤 <tkrhkfld123@naver.com>
2024-06-07 02:23:46 +0900

    feat: 리뷰의 총 개수와 평균 점수를 계산하는 함수 추가

--- src/hooks/useProductsWithMinPrice.ts
+++ src/hooks/useProductsWithMinPrice.ts
@@ -1,35 +1,66 @@
-// hooks/useProductsWithMinPrice.ts
+/* eslint-disable @typescript-eslint/no-shadow */
 import { useMemo } from 'react';
 
 const useProductsWithMinPrice = (
   products: any[],
   optionAll: any[],
+  reviews: any[],
   search: string,
 ) => {
-  const calculateMinPrice = (productName: string, options: any[]) => {
+  // 최소 가격을 계산하는 함수
+  const calculateMinPrice = (productId: number, options: any[]) => {
     const filteredOptions = options.filter(
-      (option) => option.product.name === productName,
+      (option) => option.productId === productId,
     );
     const prices = filteredOptions.map((option) => option.optionPrice);
     return prices.length ? Math.min(...prices) : 0;
   };
 
-  const productsWithMinPrice = useMemo(() => {
+  // 리뷰의 총 개수와 평균 점수를 계산하는 함수
+  const calculateReviews = (
+    productId: number,
+    options: any[],
+    reviews: any[],
+  ) => {
+    const optionIds = options
+      .filter((option) => option.productId === productId)
+      .map((option) => option.id);
+    const relevantReviews = reviews.filter((review) =>
+      optionIds.includes(review.productOptionId),
+    );
+    const totalReviews = relevantReviews.length;
+    const averageScore =
+      totalReviews > 0
+        ? relevantReviews.reduce((acc, curr) => acc + curr.score, 0) /
+          totalReviews
+        : 0;
+
+    return { totalReviews, averageScore };
+  };
+
+  const productsWithMinPriceAndReviews = useMemo(() => {
     return products
       .map((product) => {
-        const minPrice = calculateMinPrice(product.name, optionAll);
-        return { ...product, minPrice };
+        const minPrice = calculateMinPrice(product.id, optionAll);
+        const { totalReviews, averageScore } = calculateReviews(
+          product.id,
+          optionAll,
+          reviews,
+        );
+        return { ...product, minPrice, totalReviews, averageScore };
       })
       .filter((product) => {
-        const nameMatch = product.name.toLowerCase().includes(search);
+        const nameMatch = product.name
+          .toLowerCase()
+          .includes(search.toLowerCase());
         const addressMatch = product.productAddress
           .toLowerCase()
-          .includes(search);
+          .includes(search.toLowerCase());
         return nameMatch || addressMatch;
       });
-  }, [products, optionAll, search]);
+  }, [products, optionAll, reviews, search]);
 
-  return productsWithMinPrice;
+  return productsWithMinPriceAndReviews;
 };
 
 export default useProductsWithMinPrice;
