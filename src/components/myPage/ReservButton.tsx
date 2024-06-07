31dbb3ae7c611cabfe90836147a6374a6ab5355d
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-07 22:48:15 +0900

    Refactor: 예약 목록 버튼 디자인 적용

--- src/components/myPage/ReservButton.tsx
+++ src/components/myPage/ReservButton.tsx
@@ -1,16 +1,34 @@
+import { ReactComponent as LinkIcon } from '@/assets/icons/arrow-up-right.svg';
+import { ReactComponent as ReviewIcon } from '@/assets/icons/review.svg';
+import { useState } from 'react';
+
 interface ReservButtonProps {
   status: string;
   onClick?: () => void;
 }
 
 const ReservButton = ({ status, onClick }: ReservButtonProps) => {
+  const [isHover, setIsHover] = useState(false);
   return (
-    <div
-      className="flex flex-row px-12 py-8 gap-4 text-14 font-semibold"
+    <button
+      type="button"
+      className="flex flex-row items-center px-12 py-8 gap-4 text-14 hover:text-blue-6 font-semibold"
       onClick={onClick}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
-      {status === 'rejected' ? '거절 사유 보기' : '취소하기'}
-    </div>
+      {status === 'rejected' ? (
+        <>
+          거절 사유 보기
+          <LinkIcon stroke={isHover ? '#3065e8' : 'black'} />
+        </>
+      ) : (
+        <>
+          후기 쓰기
+          <ReviewIcon stroke={isHover ? '#3065e8' : 'black'} />
+        </>
+      )}
+    </button>
   );
 };
 export default ReservButton;
