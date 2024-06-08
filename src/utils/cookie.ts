ffc27504ba1577f9c093de58f063da1e6e94b364
Hooni07 <gkxogns21@gmail.com>
2024-06-08 12:27:33 +0900

    Fix: accessToken 유효기간 6시간으로 설정

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -6,7 +6,7 @@ const cookies = new Cookies();
 export const setCookie = (name: string, value: string, options?: any) => {
   return cookies.set(name, value, {
     path: '/',
-    expires: new Date(Date.now() + 3600 * 1000),
+    expires: new Date(Date.now() + 6 * 60 * 60 * 1000),
     ...options,
   });
 };
