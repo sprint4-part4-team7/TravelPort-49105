7af2b1d01ef490b37b48dedf7af07b2479509f3c
byunjiin <129635857+byunjiin@users.noreply.github.com>
2024-06-08 10:15:57 +0900

    [Feature] checkbutton (#79)
    * Feat: CheckButton 기능 구현
    
    * Refator: 로컬스토리지에 저장되는 방식으로 바꿔서 Category구현
    
    * Feat: CheckButton 기능 구현
    
    * Refator: 로컬스토리지에 저장되는 방식으로 바꿔서 Category구현
    
    ---------
    
    Co-authored-by: byunjiin <injibyun8331@gmail.com>

--- src/pages/productRegist/CheckButton.tsx
+++ src/pages/productRegist/CheckButton.tsx
@@ -1,8 +1,19 @@
-const CheckButton = () => {
+import React from 'react';
+import Button from '@/components/common/Button';
+
+type ButtonProps = {
+  disabled: boolean;
+};
+const CheckButton = ({ disabled }: ButtonProps) => {
   return (
-    <button type="submit" className="">
-      다음
-    </button>
+    <div className="absolute bottom-24 mx-40 w-[calc(100%-364px)]">
+      <Button
+        variant="default"
+        buttonStyle="text-16 p-16"
+        text="다음"
+        disabled={disabled}
+      />
+    </div>
   );
 };
 
--- src/pages/productRegist/ProductRegist.tsx
+++ src/pages/productRegist/ProductRegist.tsx
@@ -1,6 +1,5 @@
 // 상품등록페이지 메인
-
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Category from './Category';
 import Description from './Description';
 import Location from './Location';
@@ -8,17 +7,16 @@ import Date from './Date';
 import Option from './Option';
 
 const ProductRegist = () => {
-  const [categoryId, setCategoryId] = useState(0);
-  const [page, setPage] = useState(
-    <Category categoryId={categoryId} setCategoryId={setCategoryId} />,
-  );
+  const [page, setPage] = useState<React.ReactNode>();
 
-  console.log(categoryId);
+  useEffect(() => {
+    setPage(<Category setPage={setPage} />);
+  }, []);
   return (
     <>
       <aside
         id="default-sidebar"
-        className="fixed top-0 left-0 z-40 w-177 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        className="fixed top-0 left-0 z-40 w-284 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
@@ -26,14 +24,7 @@ const ProductRegist = () => {
             <li>
               <div
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                onClick={() =>
-                  setPage(
-                    <Category
-                      categoryId={categoryId}
-                      setCategoryId={setCategoryId}
-                    />,
-                  )
-                }
+                onClick={() => setPage(<Category setPage={setPage} />)}
               >
                 <div className="step-number">1</div>
                 <span className="ms-3">상품 유형</span>
@@ -81,7 +72,7 @@ const ProductRegist = () => {
         </div>
       </aside>
       <main>
-        <div className="p-4 sm:ml-177">{page}</div>
+        <div className="p-4 sm:ml-284">{page}</div>
       </main>
     </>
   );
