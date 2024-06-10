31fc4d918ed4c9b6f3626b4ea4c1b31d66bd0de3
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-10 16:39:48 +0900

    Refactor: 타입 분리, 버킷 상수 추가, 이미지 업로드 예시 추가

--- src/apis/image.ts
+++ src/apis/image.ts
@@ -1,46 +1,56 @@
+import { PostData, PresignedUrl } from '@/constants/types';
 import instance from '@/utils/axios';
 import axios from 'axios';
 
-interface PresignedUrl {
-  url: string;
-  uniqueFileName: string;
-  originalFileName: string;
-}
-
 // presigned URL을 사용하여 S3에 파일을 업로드하는 함수
 const putImageS3 = async (url: string, file: File) => {
-  const response = await axios.put(url, file);
-  return response.data;
+  try {
+    const response = await axios.put(url, file);
+    return response.data;
+  } catch (e) {
+    throw new Error('Failed to upload image to S3');
+  }
+};
+// presigned URL을 요청하는 함수
+const postImageDatas = async (data: PostData) => {
+  try {
+    const response = await instance.post('utility/presigned-urls', data);
+    if (!response.data) throw new Error('Failed to get presigned URL');
+    return response.data.presignedUrls;
+  } catch (e) {
+    console.log(e);
+    throw new Error('Failed to get presigned URL');
+  }
 };
 
 // 이미지 파일들을 받아 presigned URL을 요청하고, 해당 URL을 사용하여 S3에 업로드하는 함수
-const postImage = async (data: File[], bucket: string) => {
+const postImages = async (data: File[], bucket: string) => {
   const imageDatas = data.map((file) => {
     return {
       objectKey: file.name,
       contentType: file.type,
     };
   });
-  const response = await instance.post('utility/presigned-urls', {
+  // presigned URL 요청
+  const response = await postImageDatas({
     items: imageDatas,
     bucketName: bucket,
   });
-  if (!response.data) throw new Error('Failed to get presigned URL');
-  const { presignedUrls } = response.data;
   // 파일명을 지정된 이름으로 변경
   await Promise.all(
-    presignedUrls.map(async (responseData: PresignedUrl, index: number) => {
+    response.map(async (urlData: PresignedUrl, index: number) => {
       const blob = data[index].slice(0, data[index].size, data[index].type);
-      const newFile = new File([blob], responseData.uniqueFileName, {
+      const newFile = new File([blob], urlData.uniqueFileName, {
         type: data[index].type,
       });
       // S3에 파일 업로드
-      await putImageS3(responseData.url, newFile);
+      await putImageS3(urlData.url, newFile);
     }),
   );
-  return presignedUrls.map((res: PresignedUrl) => {
+  // 업로드된 파일의 URL들을 배열로 반환
+  return response.map((res: PresignedUrl) => {
     return res.url.split('?')[0];
   });
 };
 
-export default postImage;
+export default postImages;
---
+++ src/constants/bucket.ts
@@ -0,0 +1,10 @@
+const BUCKER_NAME = {
+  PROFILE: 'profile-images-7team',
+  REVIEW: 'review-7team',
+  PRODUCT_PREVIEW: 'product-preview-7team',
+  ADDITIONAL_PRODUCT: 'additional-product-images-7team',
+  PRODUCT_OPTION: 'product-option-image-7team',
+  TEST: 'member-profile-img',
+};
+
+export default BUCKER_NAME;
