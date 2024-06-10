336edeca8667dda59ea07907737709a262d85aaf
eugene9851 <eugene9851@naver.com>
2024-06-10 16:12:32 +0900

    Feat: 가격대 필터링

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -4,7 +4,7 @@
 import useSearchData from '@/hooks/useSearchData';
 import { useState } from 'react';
 import useProductAll from '@/hooks/reactQuery/product/useProductAll';
-import useScoreAvg from '@/hooks/useScoreAvg';
+// import useScoreAvg from '@/hooks/useScoreAvg';
 import uniqueProduct from '@/utils/uniqueProduct';
 import arrowDown from '@/assets/icons/arrowDown.svg';
 import search from '@/assets/icons/search.svg';
@@ -14,6 +14,7 @@ import Layout from '@/components/common/layout/Layout';
 import HotelCard from '@/components/common/card/HotelCard';
 import CalendarCustom from '@/components/common/CalendarCustom';
 import HeadCount from '@/components/common/filter/HeadCount';
+import PriceRange from '@/components/common/filter/PriceRange';
 
 const List = () => {
   const [filteredData, setFilteredData] = useState();
@@ -54,7 +55,7 @@ const List = () => {
 
   return (
     <Layout main noSearch={false}>
-      <div className="relative rounded-42 px-48 py-20 shadow-[0_0_12px_0_rgba(0,0,0,0.25)] mt-40 max-w-928 min-w-460 mx-auto">
+      <div className="relative rounded-42 px-48 py-20 shadow-[0_0_12px_0_rgba(0,0,0,0.25)] mt-40 max-w-928 min-w-460 mx-auto mb-100">
         <div className="flex gap-30 justify-between items-center cursor-pointer">
           {filterings.map((filtering) => (
             <p
@@ -82,6 +83,11 @@ const List = () => {
           {filterTab === '인원수' && (
             <div className="absolute w-2/5 top-80 left-0 mobile:w-full">
               <HeadCount />
+            </div>
+          )}
+          {filterTab === '가격대' && (
+            <div className="absolute w-3/5 top-80 left-0 tablet:w-full mobile:w-full">
+              <PriceRange />
             </div>
           )}
 
@@ -112,7 +118,7 @@ const List = () => {
       </div>
       <div className="flex flex-col gap-24">
         {cards.map((item) => {
-          const { avg, length } = useScoreAvg(item.id);
+          // const { avg, length } = useScoreAvg(item.id);
 
           return (
             <HotelCard
@@ -120,8 +126,8 @@ const List = () => {
               title={item.product.name}
               location={item.product.productAddress}
               price={item.optionPrice}
-              score={avg}
-              review={length}
+              score={1}
+              review={5}
               image={item.product.productImages[0]}
               link="/"
             />
