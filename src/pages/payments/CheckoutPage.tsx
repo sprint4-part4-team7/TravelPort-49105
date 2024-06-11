7b4fcfcbf13bef96d96ee7da019b9678ab9d01fc
박지윤 <tkrhkfld123@naver.com>
2024-06-12 04:19:50 +0900

    style: 결제 페이지 디자인 구현 완료

--- src/pages/payments/CheckoutPage.tsx
+++ src/pages/payments/CheckoutPage.tsx
@@ -1,7 +1,12 @@
-import usePaymentWidget from '@/hooks/usePaymentWidget';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
 import './Payments.css';
+import usePaymentWidget from '@/hooks/usePaymentWidget';
 import useProductOptionQuery from '@/hooks/reactQuery/product/useProductionOptionQuery';
 import useTilmeTabaleOptionQuery from '@/hooks/reactQuery/timeTable/useTilmeTabaleOptionQuery';
+import minusPay from '@/assets/icons/minusPay.svg';
+import plusPay from '@/assets/icons/plusPay.svg';
 import Layout from '@/components/common/layout/Layout';
 import Footer from '@/components/common/Footer';
 import Pay from '@/components/Pay';
@@ -24,27 +29,107 @@ const CheckoutPage = () => {
     isLoading,
     error,
   } = useProductOptionQuery(optionId);
-  // console.log(productOptionData);
+
+  const [count, setCount] = useState(productOptionData?.userCount || 1);
+  const optionPrice = productOptionData?.optionPrice || 0;
+  const maxUserCount = productOptionData?.maxUserCount || Infinity;
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
 
   const { data: timeTableOptionData } = useTilmeTabaleOptionQuery(optionId);
-  console.log(timeTableOptionData);
 
   if (isLoading) return <h1>loading...</h1>;
   if (error) return <h1>error...</h1>;
 
   return (
-    <div className="flex flex-col items-center w-full p-6 overflow-auto">
+    <>
       <Layout>
-        <div className="p-10 text-2xl text-white bg-pink-600 w-570 rounded-2xl">
-          상품이름: {productOptionData?.product.name}
+        <div className="border-b-2 border-solid border-black-12 mt-100">
+          <div className="px-8 py-16 font-semibold text-22">1. 결제확인</div>
         </div>
-        <div className="p-10 text-2xl text-white bg-pink-600 w-570 rounded-2xl">
-          가격: {productOptionData?.optionPrice}원
+
+        <div className="flex items-center py-16 border-solid border-b-1 border-black-3">
+          <div className="flex justify-between w-full mobile:flex-col">
+            <div className="flex">
+              {/* 체크박스 */}
+              <div className="flex items-center justify-center mr-12">
+                <input type="checkbox" className="w-20 h-20" />
+              </div>
+              {/* 상품 이름, 옵션, 날짜 */}
+              <div className="flex flex-col flex-grow">
+                <div className="mb-6 font-normal text-18">
+                  {productOptionData?.product.name}
+                </div>
+                <div className="flex flex-row items-start gap-12 mobile:flex-col">
+                  <div className="flex flex-row items-start gap-8">
+                    <div className="font-normal text-16">옵션</div>
+                    <div className="px-8 py-4 font-normal border-solid text-11 bg-black-3 border-1 border-black-5 rounded-4">
+                      {productOptionData?.optionName}
+                    </div>
+                  </div>
+                  <div className="flex flex-row items-start gap-8">
+                    <div className="font-normal text-16">날짜</div>
+                    <div className="px-8 py-4 font-normal border-solid text-11 bg-black-3 border-1 border-black-5 rounded-4">
+                      24.06.12 ~ 24.06.13
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* 카운트와 가격 (모바일 화면에서만 아래로 이동) */}
+            <div className="flex items-center ml-auto gap-60 mobile:justify-between mobile:w-full mobile:mt-40">
+              <div className="flex gap-16 px-6 py-4 border-solid border-1 border-black-4 rounded-4">
+                <button
+                  type="button"
+                  onClick={decreaseCount}
+                  className="outline-none"
+                  disabled={count <= 1}
+                >
+                  <img src={minusPay} alt="마이너스 아이콘" />
+                </button>
+                <div className="font-normal text-16">{count}</div>
+                <button
+                  type="button"
+                  onClick={increaseCount}
+                  className="outline-none"
+                  disabled={count >= maxUserCount}
+                >
+                  <img src={plusPay} alt="플러스 아이콘" />
+                </button>
+              </div>
+              <div className="flex font-semibold text-18 ">
+                {(optionPrice * count).toLocaleString()}원
+              </div>
+            </div>
+          </div>
         </div>
-        <Pay requestPayment={requestPayment} />
+
+        <div className="flex justify-end gap-20 font-semibold mt-28 text-17 mb-100">
+          <div>최종 결제 금액</div>
+          <div className="ml-2">{(optionPrice * count).toLocaleString()}원</div>
+        </div>
+
+        <div className="border-b-2 border-solid border-black-12">
+          <div className="px-8 py-16 font-semibold text-22">2. 결제수단</div>
+        </div>
+
+        <div className="flex flex-col items-center w-full ">
+          <div className="w-570 mobile:w-400">
+            <Pay requestPayment={requestPayment} />
+          </div>
+        </div>
       </Layout>
       <Footer />
-    </div>
+    </>
   );
 };
 
