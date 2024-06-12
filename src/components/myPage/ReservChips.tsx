ae4a5a5b086bea2c7c155a53a65f63c18ec30ab0
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-06-12 21:39:56 +0900

    [Fix] Card내 날짜 출력 방식 변경을 위한 함수 정의 및 적용(0000.00.00), ReservChips 반환 형식 변경 (#96)
    * Feat: 날짜 형태를 통일 시키기 위한 함수 구현
    
    * Fix: 우측 상단 예약 상태를 나타내는 컴포넌트 적용 및 사용하지 않는 코드 제거
    
    * Fix: 하나의 이미지로 사용하는 것 수정(icon + text 형태로 수정), ReserveStatusType 수정
    
    * Feat: useUserStore를 활용하여 접속한 partnerId 파악 및 적용

--- src/components/myPage/ReservChips.tsx
+++ src/components/myPage/ReservChips.tsx
@@ -1,25 +1,44 @@
-import approved from '@/assets/icons/status-approved.svg';
-import pending from '@/assets/icons/status-pending.svg';
-import rejected from '@/assets/icons/status-rejected.svg';
+import approved from '@/assets/icons/status-chip-approved.svg';
+import pending from '@/assets/icons/status-chip-pending.svg';
+import rejected from '@/assets/icons/status-chip-rejected.svg';
 
 const ReservChips = ({ status }: { status: string }) => {
   let src;
+  let style =
+    'flex justify-center items-center gap-4 px-8 py-4 text-14 font-semibold ';
+  let text = '';
   switch (status) {
-    case 'approved':
+    case '예약 완료':
       src = approved;
+      style += 'text-blue-6';
+      text = '수락됨';
       break;
-    case 'pending':
+    case '예약 대기':
       src = pending;
+      style += 'text-system-warning';
+      text = '대기중';
       break;
-    case 'rejected':
+    case '예약 거절':
       src = rejected;
+      style += 'text-system-error';
+      text = '거절됨';
+      break;
+    case '예약 취소':
+      src = rejected;
+      style += 'text-system-error';
+      text = '취소됨';
       break;
     default:
       src = '';
       break;
   }
 
-  return <img src={src} alt={status} />;
+  return (
+    <div className={style}>
+      <img src={src} alt={status} />
+      {text}
+    </div>
+  );
 };
 
 export default ReservChips;
