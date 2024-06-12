cc1664c603f181b086313643fb4689fa69d60261
박지윤 <tkrhkfld123@naver.com>
2024-06-12 20:07:21 +0900

    chore: product, productOption, review 계산 공식 제거

--- src/hooks/useProductsWithMinPrice.ts
+++ src/hooks/useProductsWithMinPrice.ts
@@ -1,76 +1,22 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 import { useMemo } from 'react';
 
-const useProductsWithMinPrice = (
-  products: any,
-  optionAll: any,
-  reviews: any,
-  search: string,
-) => {
-  // 최소 가격을 계산하는 함수
-  const calculateMinPrice = (productId: number, options: any) => {
-    // 옵션 존재 여부 확인
-    if (!options) return 0;
-    const filteredOptions =
-      options &&
-      options.filter((option: any) => option.productId === productId);
-    const prices =
-      filteredOptions &&
-      filteredOptions.map((option: any) => option.optionPrice);
-    return prices.length ? Math.min(...prices) : 0;
-  };
+const useProductsWithMinPrice = (products: any, search: string) => {
+  const matedProductsSearch = useMemo(() => {
+    // 서치바랑 이름 또는 주소 같은지 확인
+    if (!products) return [];
+    return products.filter((product: any) => {
+      const nameMatch =
+        product.productName &&
+        product.productName.toLowerCase().includes(search.toLowerCase());
+      const addressMatch =
+        product.productAddress &&
+        product.productAddress.toLowerCase().includes(search.toLowerCase());
+      return nameMatch || addressMatch;
+    });
+  }, [products, search]);
 
-  // 리뷰의 총 개수와 평균 점수를 계산하는 함수
-  const calculateReviews = (
-    productId: number,
-    options: any,
-    reviews: any[],
-  ) => {
-    // 옵션과 리뷰의 존재 여부 확인
-    if (!options || !reviews) return { totalReviews: 0, averageScore: 0 };
-    const optionIds =
-      options &&
-      options
-        .filter((option: any) => option.productId === productId)
-        .map((option: any) => option.id);
-    const relevantReviews =
-      reviews &&
-      reviews.filter((review) => optionIds.includes(review.productOptionId));
-    const totalReviews = relevantReviews.length;
-    const averageScore =
-      totalReviews > 0
-        ? relevantReviews.reduce((acc, curr) => acc + curr.score, 0) /
-          totalReviews
-        : 0;
-
-    return { totalReviews, averageScore };
-  };
-
-  const productsWithMinPriceAndReviews = useMemo(() => {
-    // 제품, 옵션, 리뷰 존재 여부 확인
-    if (!products || !optionAll || !reviews) return [];
-    return products
-      .map((product: any) => {
-        const minPrice = calculateMinPrice(product.id, optionAll);
-        const { totalReviews, averageScore } = calculateReviews(
-          product.id,
-          optionAll,
-          reviews,
-        );
-        return { ...product, minPrice, totalReviews, averageScore };
-      })
-      .filter((product: any) => {
-        const nameMatch =
-          product.name &&
-          product.name.toLowerCase().includes(search.toLowerCase());
-        const addressMatch =
-          product.productAddress &&
-          product.productAddress.toLowerCase().includes(search.toLowerCase());
-        return nameMatch || addressMatch;
-      });
-  }, [products, optionAll, reviews, search]);
-
-  return productsWithMinPriceAndReviews;
+  return matedProductsSearch;
 };
 
 export default useProductsWithMinPrice;
