b8f9b9ea8f845a072699bbe01636aeaf5edef357
byunjiin <injibyun8331@gmail.com>
2024-06-12 18:17:59 +0900

    Feat: 버튼 로직구현 및 aside페이지 연결 완료

--- src/pages/productRegist/CheckButton.tsx
+++ src/pages/productRegist/CheckButton.tsx
@@ -3,8 +3,9 @@ import Button from '@/components/common/Button';
 
 type ButtonProps = {
   disabled: boolean;
+  onClick?: () => void;
 };
-const CheckButton = ({ disabled }: ButtonProps) => {
+const CheckButton = ({ disabled, onClick }: ButtonProps) => {
   return (
     <div className="absolute bottom-24 mx-40 w-[calc(100%-364px)]">
       <Button
@@ -12,6 +13,7 @@ const CheckButton = ({ disabled }: ButtonProps) => {
         variant="default"
         buttonStyle="text-16 p-16"
         disabled={disabled}
+        onClick={onClick}
       >
         다음
       </Button>
