2d8199891a5db6c1d30580d8befabf60dbd20f5d
Hooni07 <gkxogns21@gmail.com>
2024-06-12 20:24:07 +0900

    Feat: 날짜 형태를 통일 시키기 위한 함수 구현

---
+++ src/utils/changeDateForm.ts
@@ -0,0 +1,10 @@
+const changeDateForm = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}.${month}.${day}`;
+};
+
+export default changeDateForm;
