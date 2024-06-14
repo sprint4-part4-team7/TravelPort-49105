467953693586a2cb71e1139d36a46bf39593e560
byunjiin <injibyun8331@gmail.com>
2024-06-15 02:44:57 +0900

    Style: 이미지 모달 세부 CSS

--- src/pages/productRegist/ImgModal.tsx
+++ src/pages/productRegist/ImgModal.tsx
@@ -1,11 +1,13 @@
 import { useForm } from 'react-hook-form';
-import plusImage from '@/assets/icons/plus.svg';
+import plusImage from '@/assets/icons/plus-blue.svg';
 import trashImage from '@/assets/icons/trash-red.svg';
 import { useEffect, useState } from 'react';
+import Button from '@/components/common/Button';
 
 type ImageForm = {
   img: FileList;
   thumbnail: FileList;
+  check: FileList;
 };
 const ImgModal = () => {
   const { register, handleSubmit, watch } = useForm<ImageForm>({
@@ -14,6 +16,8 @@ const ImgModal = () => {
   const [imageArray, setImageArray] = useState<any>([]);
 
   const imageData = watch('img');
+
+  const trueButton = true;
 
   useEffect(() => {
     if (imageData !== undefined) {
@@ -28,7 +32,7 @@ const ImgModal = () => {
         if (temp.length <= 5) {
           setImageArray(Array.from(temp));
         } else {
-          alert('최대 5게');
+          alert('최대 5개');
         }
       }
     }
@@ -40,27 +44,34 @@ const ImgModal = () => {
     }
     return imageArray.map((file: any, index: number) => {
       return (
-        <div key={file.name} className="flex items-center">
-          <img
-            className="w-40 h-40"
-            src={URL.createObjectURL(file)}
-            alt="상품옵션"
-          />
-          <p>{file.name}</p>
-          <label className="flex" htmlFor="check">
-            <input id="check" type="radio" />
-            대표
-          </label>
-          <img
-            src={trashImage}
-            alt="삭제 아이콘"
-            role="presentation"
-            onClick={() => {
-              const temp = [...imageArray];
-              temp.splice(index, 1); // 배열에서 선택한 인덱스를 삭제해서 배열을 재정의
-              setImageArray(temp); // 재정의된 배열을 set안에 넣어서 재정의+state변환
-            }}
-          />
+        <div
+          key={file.name}
+          className="flex  bg-white border border-black-4 items-center p-12 rounded justify-between"
+        >
+          <div className="flex gap-12 items-center">
+            <img
+              className="w-40 h-40"
+              src={URL.createObjectURL(file)}
+              alt="상품옵션"
+            />
+            <p>{file.name}</p>
+          </div>
+          <div className="flex gap-12 items-center">
+            <label className="flex gap-8" htmlFor="check">
+              <input id="check" type="radio" {...register('check')} />
+              대표
+            </label>
+            <img
+              src={trashImage}
+              alt="삭제 아이콘"
+              role="presentation"
+              onClick={() => {
+                const temp = [...imageArray];
+                temp.splice(index, 1); // 배열에서 선택한 인덱스를 삭제해서 배열을 재정의
+                setImageArray(temp); // 재정의된 배열을 set안에 넣어서 재정의+state변환
+              }}
+            />
+          </div>
         </div>
       );
     });
@@ -73,25 +84,40 @@ const ImgModal = () => {
   return (
     <div className="p-16 w-384 h-532">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="border">
-          <label htmlFor="imgPlus">
-            <img src={plusImage} alt="플러스 아이콘" />
-            <p>이미지 업로드 (최대 5장)</p>
-            <input
-              className="hidden"
-              id="imgPlus"
-              {...register('img')}
-              type="file"
-              accept="image/*"
-              multiple
-            />
-          </label>
+        <label
+          className="flex items-center justify-center gap-12 border border-dashed border-blue-6 rounded text-blue-6 p-12"
+          htmlFor="imgPlus"
+        >
+          <img src={plusImage} alt="플러스 아이콘" />
+          <p className="text-15">이미지 업로드 (최대 5장)</p>
+          <input
+            className="hidden"
+            id="imgPlus"
+            {...register('img')}
+            type="file"
+            accept="image/*"
+            multiple
+          />
+        </label>
+        <div className="absolute bottom-32 flex gap-12 items-center">
+          <div className="w-166">
+            <Button
+              buttonStyle="h-28"
+              outlined={trueButton}
+              buttonType="button"
+            >
+              취소
+            </Button>
+          </div>
+          <div className="w-166">
+            <Button buttonStyle="h-28" buttonType="submit">
+              완료
+            </Button>
+          </div>
         </div>
-        <button type="button">취소</button>
-        <button type="submit">확인</button>
       </form>
-      <div>
-        <p>이미지</p>
+      <div className="bg-black-3 rounded flex flex-col gap-8 p-12 my-10">
+        <p className="text-14">이미지</p>
         {showImage()}
       </div>
     </div>
