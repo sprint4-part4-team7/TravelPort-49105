b94fdcfa6fbfd1a13ebe7801a053506d56514de1
Hooni07 <gkxogns21@gmail.com>
2024-06-14 21:39:52 +0900

    Fix: 변경된 useSocialLoginQuery 방식 각 소셜 사이트(google, kakao, naver)별로 변경 적용

--- src/pages/auth/login/NaverRedirect.tsx
+++ src/pages/auth/login/NaverRedirect.tsx
@@ -1,19 +1,17 @@
-import { useNaverLoginMutation } from '@/hooks/reactQuery/auth/useSocialLoginMutation';
-import { useEffect } from 'react';
+import { useNaverLoginQuery } from '@/hooks/reactQuery/auth/useSocialLoginQuery';
+import Loading from '@/components/common/Loading';
 
 const NaverRedirect = () => {
   const code = new URL(window.location.href).searchParams.get('code');
-  const { mutate } = useNaverLoginMutation();
-
-  useEffect(() => {
-    if (code) {
-      mutate(code);
-    }
-  }, [code, mutate]);
+  const { isLoading } = useNaverLoginQuery(code);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Naver Redirect Page</h1>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <h1 className="text-36 font-bold underline">Naver Redirect Page</h1>
+      )}
     </div>
   );
 };
