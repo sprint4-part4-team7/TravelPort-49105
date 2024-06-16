3f45c905091584b46031e38f191b9934c2bf7d22
박지윤 <tkrhkfld123@naver.com>
2024-06-16 20:14:56 +0900

    chore: 결제 실패 페이지 생성 및 적용

--- src/pages/payments/FailPage.tsx
+++ src/pages/payments/FailPage.tsx
@@ -1,40 +1,49 @@
+import { useNavigate } from 'react-router-dom';
+
+import failIcon from '@/assets/icons/noPay.svg';
+import Layout from '@/components/common/layout/Layout';
 import './Payments.css';
+import Button from '@/components/common/Button';
 
 const FailPage = () => {
+  const navigate = useNavigate();
   return (
-    <div className="w-full">
-      <div className="flex flex-col items-center w-full max-w-xs mx-auto">
-        <h2 className="text-xl font-bold">결제를 실패했어요</h2>
-        <div className="flex flex-col items-center w-full">
-          <a
-            className="px-4 py-2 my-2 font-bold text-black bg-gray-200 rounded cursor-pointer btn"
-            href="https://developers.tosspayments.com/sandbox"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            다시 테스트하기
-          </a>
-          <div className="flex gap-4">
-            <a
-              className="w-full px-4 py-2 font-bold text-black bg-gray-200 rounded cursor-pointer btn"
-              href="https://docs.tosspayments.com/reference/error-codes"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              에러코드 문서보기
-            </a>
-            <a
-              className="w-full px-4 py-2 font-bold text-black bg-gray-200 rounded cursor-pointer btn"
-              href="https://techchat.tosspayments.com"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              실시간 문의하기
-            </a>
+    <Layout>
+      <div className="w-full">
+        <div className="flex flex-col items-center w-full max-w-xl mx-auto">
+          <div className="flex flex-col items-center">
+            <img
+              className="mobile:w-48"
+              src={failIcon}
+              alt="결제 실패 아이콘"
+            />
+            <div className="flex items-center justify-center mt-32 font-bold text-32 w-564 tablet:flex-col mobile:flex-col">
+              <div>결제가 정상적으로</div>
+              <div>처리되지 않았습니다</div>
+            </div>
+            <div className="flex gap-28 mt-100 mobile:flex-col">
+              <div className="flex items-center w-320 ">
+                <Button
+                  outlined
+                  buttonStyle="w-full h-48 text-16 font-normal "
+                  onClick={() => navigate(-2)}
+                >
+                  다시 결제하기
+                </Button>
+              </div>
+              <div className="flex items-center w-320 ">
+                <Button
+                  buttonStyle="w-full  h-48 text-16 font-normal"
+                  onClick={() => navigate('/')}
+                >
+                  더 둘러보기
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
