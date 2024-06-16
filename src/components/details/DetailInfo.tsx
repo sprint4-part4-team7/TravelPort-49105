2af41017cd3475f5f1a4f28805bbd88d974c83df
eugene9851 <eugene9851@naver.com>
2024-06-16 13:21:21 +0900

    Feat: 상세페이지 상세정보 UI 수정

--- src/components/details/DetailInfo.tsx
+++ src/components/details/DetailInfo.tsx
@@ -7,17 +7,21 @@ type DetailInfoProps = {
 const DetailInfo = ({ options }: DetailInfoProps) => {
   return (
     <>
-      <h1 className="pt-16 text-20 font-semibold">소개</h1>
+      <h1 className="py-16 text-20 font-semibold">소개</h1>
       <div className="flex flex-col gap-20 pb-40">
         {options.map((option) => (
           <>
+            <h1 className="text-18 font-medium">{option.optionName}</h1>
             <p className="text-16 leading-[162%]">{option.optionDesc}</p>
+
             {option.optionImages.forEach((image) => {
-              <img
-                src={image}
-                alt="옵션이미지"
-                className="w-full object-cover"
-              />;
+              return (
+                <img
+                  src={image}
+                  alt="옵션이미지"
+                  className="w-full object-cover"
+                />
+              );
             })}
           </>
         ))}
