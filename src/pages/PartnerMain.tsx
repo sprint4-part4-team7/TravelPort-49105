481438c00c36a8b402709303a8cae612d3eea619
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-17 16:40:07 +0900

    Refactor: 새 상품 등록 경로 설정

--- src/pages/PartnerMain.tsx
+++ src/pages/PartnerMain.tsx
@@ -1,34 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import PartnerMainButton from '@/components/PartnerMainButton';
 import Layout from '@/components/common/layout/Layout';
+import Footer from '@/components/common/Footer';
 
 const PartnerMain = () => {
   const navigate = useNavigate();
 
   return (
-    <Layout noSearch category={false}>
-      <div className="flex flex-col py-16 items-center">
-        <div className="flex mobile:flex-col gap-12 mobile:w-full py-20 text-28 font-bold">
-          <span className="mobile:text-24">판매자님,</span>
-          <span className="mobile:text-22">무엇을 도와드릴까요?</span>
+    <>
+      <Layout noSearch category={false}>
+        <div className="flex flex-col py-16 items-center">
+          <div className="flex mobile:flex-col gap-12 mobile:w-full py-20 text-28 font-bold">
+            <span className="mobile:text-24">판매자님,</span>
+            <span className="mobile:text-22">무엇을 도와드릴까요?</span>
+          </div>
+          <div className="flex mobile:flex-col mobile:w-full py-20 gap-20">
+            <PartnerMainButton
+              text="새 상품 등록하기"
+              onClick={() => navigate('/partner/product-register')}
+            />
+            <PartnerMainButton
+              text="내 게시물 목록 보기"
+              onClick={() => navigate('/partner/mypage/edit-info')}
+            />
+            <PartnerMainButton
+              text="예약 관리"
+              onClick={() => navigate('/partner/mypage/posting-manage')}
+            />
+            <PartnerMainButton
+              text="판매자 정보 수정"
+              onClick={() => navigate('/partner/mypage/reserve-manage')}
+            />
+          </div>
         </div>
-        <div className="flex mobile:flex-col mobile:w-full py-20 gap-20">
-          <PartnerMainButton
-            text="내 게시물 목록 보기"
-            onClick={() => navigate('/partner/mypage/edit-info')}
-          />
-          <PartnerMainButton
-            text="예약 관리"
-            onClick={() => navigate('/partner/mypage/posting-manage')}
-          />
-          <PartnerMainButton text="내 사업자 등록증" />
-          <PartnerMainButton
-            text="판매자 정보 수정"
-            onClick={() => navigate('/partner/mypage/reserve-manage')}
-          />
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+      <Footer />
+    </>
   );
 };
 
