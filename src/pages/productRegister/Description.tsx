dfecf24c5df57105a681036b3a141f6f0d639c0b
byunjiin <injibyun8331@gmail.com>
2024-06-17 23:39:47 +0900

    Fix: description 로컬에 맞게 수정

--- src/pages/productRegister/Description.tsx
+++ src/pages/productRegister/Description.tsx
@@ -27,8 +27,9 @@ const Description = ({ setPage }: PageIdProps) => {
   const { isModalOpen, openModal, closeModal } = useModal();
 
   const onSubmit = (data: DescriptionForm) => {
+    localStorage.setItem('title', data.title);
+    localStorage.setItem('content', data.content);
     setPage(<Location setPage={setPage} />);
-    console.log(data);
   };
 
   return (
