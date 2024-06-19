899f908ea32d5e4f92305b0165265a18ce255180
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-06-19 21:01:51 +0900

    [Fix] 이메일 중복 확인 없이 회원가입 되던 문제 및 회원가입 시 Error 동작 수정, Toast 내부 text 크기 및 font 굵기 수정 (#139)
    * Fix: 회원가입 시 이메일 중복 확인 없이 회원가입 되던 문제, 이미 가입한 계정에 대한 toast 및 error 동작 수정
    
    * Design: Toast 내 text 크기 및 font 조절
    
    * Fix: 사용하지 않는 function 주석 처리
    
    * Fix: 사용하지 않는 function 삭제

--- src/pages/auth/signup/PartnerSignup.tsx
+++ src/pages/auth/signup/PartnerSignup.tsx
@@ -36,7 +36,7 @@ const PartnerSignup = () => {
     defaultValues: { company: '', email: '', password: '', passwordCheck: '' },
   });
   const navigate = useNavigate();
-  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState<boolean | null>();
   const [emailMessage, setEmailMessage] = useState('');
   const setUserInfo = useUserStore((state) => state.setUserInfo);
   const { mutate: verifyEmail } = useVerifyEmail();
@@ -76,15 +76,13 @@ const PartnerSignup = () => {
   const handleSignupForm = async (data: PartnerSignupData) => {
     const { company: name, email, password } = data;
     const loginType: UserType = 'PARTNER';
-    const signupData = { name, email, password, loginType };
+    const signupData = { name, email, password, loginType, isEmailValid };
     signUp(signupData, {
       onSuccess: () => {
-        if (isEmailValid) {
-          toast.success('정상 가입되었습니다!');
-          const accessToken = getCookie('accessToken');
-          if (accessToken) setUserInfo({ ...jwtDecode(accessToken) });
-          navigate('/login', { replace: true });
-        }
+        toast.success('정상 가입되었습니다!');
+        const accessToken = getCookie('accessToken');
+        if (accessToken) setUserInfo({ ...jwtDecode(accessToken) });
+        navigate('/login', { replace: true });
       },
     });
   };
