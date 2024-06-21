8ff1e8d596979c59989bfc8cde7bc9bbf65efeb6
byunjiin <injibyun8331@gmail.com>
2024-06-21 13:14:12 +0900

    Feat: 미리보기 페이지에서 기능구현 및 모달에 로컬기록저장

--- src/pages/productRegister/Description.tsx
+++ src/pages/productRegister/Description.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
 import uploadImage from '@/assets/icons/upload.svg';
 import useModal from '@/hooks/useModal';
+import { useThumbnailStore } from '@/utils/zustand';
 import { PageIdProps } from './productPage';
 import Location from './Location';
 import CheckButton from './CheckButton';
@@ -11,12 +13,14 @@ import ImgModal from './ImgModal';
 type DescriptionForm = {
   title: string;
   content: string;
+  img: File;
 };
 
 const Description = ({ setPage }: PageIdProps) => {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { isValid },
   } = useForm<DescriptionForm>({
     mode: 'onChange',
@@ -25,19 +29,37 @@ const Description = ({ setPage }: PageIdProps) => {
   const outlined = true;
 
   const { isModalOpen, openModal, closeModal } = useModal();
+  const { thumbnail } = useThumbnailStore();
+  const [showImg, setShowImg] = useState('');
+
+  useEffect(() => {
+    const optionTitle = localStorage.getItem('title');
+    if (optionTitle) {
+      setValue('title', optionTitle);
+    }
+    const optionContent = localStorage.getItem('content');
+    if (optionContent) {
+      setValue('content', optionContent);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (thumbnail.size > 0) {
+      setShowImg(URL.createObjectURL(thumbnail));
+    }
+  }, [thumbnail]);
 
   const onSubmit = (data: DescriptionForm) => {
     localStorage.setItem('title', data.title);
     localStorage.setItem('content', data.content);
     setPage(<Location setPage={setPage} />);
   };
-
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mx-40 flex flex-col gap-24">
           <label className="flex gap-12 flex-col" htmlFor="title">
-            <p className="text-17">제목</p>
+            <p className="font-semibold text-17">제목</p>
             <input
               className="h-48 p-12 rounded text-16 outline-none border-solid border-1 border-black-5 w-full focus:border-blue-6 focus:border-1 mobile:max-w-none"
               {...register('title', {
@@ -50,7 +72,7 @@ const Description = ({ setPage }: PageIdProps) => {
             />
           </label>
           <label className="flex gap-12 flex-col" htmlFor="content">
-            <p className="text-17">내용</p>
+            <p className="font-semibold text-17">내용</p>
             <textarea
               className="resize-none h-216 p-12 rounded text-16 outline-none border-solid border-1 border-black-5 w-full focus:border-blue-6 focus:border-1 mobile:max-w-none"
               {...register('content', {
@@ -62,12 +84,26 @@ const Description = ({ setPage }: PageIdProps) => {
             />
           </label>
           <div className="flex flex-col gap-12">
-            <p className="text-17">이미지 업로드</p>
+            <p className="font-semibold text-17">이미지 업로드</p>
             <div className="flex flex-row gap-32 items-center">
-              <div className="flex flex-col gap-8 items-center">
-                <div className="w-120 h-120 rounded-3 bg-black-4" />
+              <label className="flex flex-col gap-8 items-center" htmlFor="img">
+                {thumbnail.size > 0 ? (
+                  <img
+                    className="w-120 h-120 rounded-3"
+                    src={showImg}
+                    alt="썸네일"
+                  />
+                ) : (
+                  <input
+                    {...register('img', {
+                      required: true,
+                    })}
+                    className="w-120 h-120 rounded-3 bg-black-4"
+                    id="img"
+                  />
+                )}
                 <p className="text-14">대표 이미지</p>
-              </div>
+              </label>
               <div className="w-153">
                 <Button
                   buttonType="button"
