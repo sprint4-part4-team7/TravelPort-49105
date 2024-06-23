f68880c9be33591d07a7d72a8ba984a3045ab46f
박지윤 <tkrhkfld123@naver.com>
2024-06-23 15:18:27 +0900

    style: 헤더 바뀐 부분 1차 수정

--- src/components/MainCategoryButton.tsx
+++ src/components/MainCategoryButton.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import search from '@/assets/icons/headerSearchButton.svg';
 
 interface MainCategoryButtonprops {
   title: string;
   onclick: () => void;
+  className?: string;
 }
 
 const MainCategoryButton: React.FC<MainCategoryButtonprops> = ({
   title,
   onclick,
+  className,
 }) => {
   return (
     <button
-      className="px-16 py-6 border-solid mobile:px-8 mobile:py-6 border-1 border-black-4 rounded-18 mobile:border-none tablet:px-10"
+      className={`px-16 py-6 mobile:px-12 mobile:mt-10 mobile:py-6 tablet:px-10 ${className}`}
       type="button"
       onClick={onclick}
     >
       <div className="flex gap-8 tablet:gap-3">
-        <p className="font-normal text-black-8 text-14 mobile:hidden tablet:text-12">
+        <p className="font-normal text-black-8 text-14 tablet:text-12 w-30">
           {title}
         </p>
-        <p className="font-normal tablet:hidden desktop:hidden text-black-8 text-11 mobile:flex ">
-          # {title}
-        </p>
-        <img className="mobile:hidden" src={search} alt="돋보기 아이콘" />
       </div>
     </button>
   );
