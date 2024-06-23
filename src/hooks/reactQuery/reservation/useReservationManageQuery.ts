cd15a264bf412429bbe904a68587890879446a55
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-06-23 18:40:52 +0900

    [Docs & Fix] README.md 작성 및  (#159)
    * Docs: README.md 작성
    
    * Fix: 예약 관리 api 변경에 따른 코드 수정
    
    * Fix: 불필요한 파일 제거
    
    * Docs: README.md 이름순 수정 및 변지인 내용 추가
    
    * Docs: README.md 디자이너 프로필 추가
    
    * Fix: 머지 이슈 해결

--- src/hooks/reactQuery/reservation/useReservationManageQuery.ts
+++ src/hooks/reactQuery/reservation/useReservationManageQuery.ts
@@ -4,19 +4,40 @@ import reservationApi from '@/apis/reservation';
 interface GetReservationManageProps {
   partnerId: number;
   categoryId: number;
+  offset: number;
+  limit: number;
+  isNew: boolean;
+}
+
+interface ReservProps {
+  reservations: any[];
+  totalCount: number;
 }
 
 const useReservationManageQuery = ({
   partnerId,
   categoryId,
+  offset,
+  limit,
+  isNew,
 }: GetReservationManageProps) => {
+  const order = isNew ? 'DESC' : 'ASC';
   const { data, isLoading, error } = useQuery({
-    queryKey: [partnerId, categoryId],
+    queryKey: [partnerId, categoryId, offset, isNew],
     queryFn: () =>
-      reservationApi.getReservationManage({ partnerId, categoryId }),
+      reservationApi.getReservationManage({
+        partnerId,
+        categoryId,
+        offset,
+        limit,
+        order,
+      }),
   });
 
-  const reservedData = data?.data ?? [];
+  const reservedData: ReservProps = data?.data ?? {
+    reservations: [],
+    totalCount: 0,
+  };
 
   return { reservedData, isLoading, error };
 };
